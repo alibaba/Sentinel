f532e041d7a8ecd75cf6277e437a96c84481c860
Eric Zhao <sczyh16@gmail.com>
2018-07-30 16:42:56 +0800

    Add sample for Sentinel Nacos DataSource
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

---
+++ sentinel-demo/sentinel-demo-nacos-datasource/src/main/java/com/alibaba/csp/sentinel/demo/datasource/nacos/NacosDataSourceDemo.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.csp.sentinel.demo.datasource.nacos;
+
+import java.util.List;
+
+import com.alibaba.csp.sentinel.datasource.DataSource;
+import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+
+/**
+ * This demo demonstrates how to use Nacos as the data source of Sentinel rules.
+ *
+ * Before you start, you need to start a Nacos server in local first, and then
+ * use {@link NacosConfigSender} to publish initial rule configuration to Nacos.
+ *
+ * @author Eric Zhao
+ */
+public class NacosDataSourceDemo {
+
+    private static final String KEY = "TestResource";
+
+    public static void main(String[] args) {
+        loadRules();
+        // Assume we config: resource is `TestResource`, initial QPS threshold is 5.
+        FlowQpsRunner runner = new FlowQpsRunner(KEY, 1, 100);
+        runner.simulateTraffic();
+        runner.tick();
+    }
+
+    private static void loadRules() {
+        final String remoteAddress = "localhost";
+        final String groupId = "Sentinel:Demo";
+        final String dataId = "com.alibaba.csp.sentinel.demo.flow.rule";
+
+        DataSource<String, List<FlowRule>> flowRuleDataSource = new NacosDataSource<>(remoteAddress, groupId, dataId,
+            source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {}));
+        FlowRuleManager.register2Property(flowRuleDataSource.getProperty());
+    }
+}
