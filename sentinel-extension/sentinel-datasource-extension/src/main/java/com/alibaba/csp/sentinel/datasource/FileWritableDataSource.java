641a93d06f7d2d64f6de7cc674435c5afa04a82e
Eric Zhao <sczyh16@gmail.com>
2018-09-04 11:00:20 +0800

    Some refactor of data source
    - Add `close` method in WritableDataSource (to extend AutoCloseable in JDK 1.7 later)
    - Separate the writable file data source from original class
    - Add a sample to show how to register data sources via Sentinel init mechanism
    - Separate a writable data source registry from original handler to make it clear
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

---
+++ sentinel-extension/sentinel-datasource-extension/src/main/java/com/alibaba/csp/sentinel/datasource/FileWritableDataSource.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.csp.sentinel.datasource;
+
+import java.io.File;
+
+/**
+ * A {@link WritableDataSource} based on file.
+ *
+ * @param <T> data type
+ * @author Eric Zhao
+ * @since 0.2.0
+ */
+public class FileWritableDataSource<T> implements WritableDataSource<T> {
+
+    private final Converter<T, String> configEncoder;
+    private File file;
+
+    public FileWritableDataSource(String filePath, Converter<T, String> configEncoder) {
+        this(new File(filePath), configEncoder);
+    }
+
+    public FileWritableDataSource(File file, Converter<T, String> configEncoder) {
+        if (file == null || file.isDirectory()) {
+            throw new IllegalArgumentException("Bad file");
+        }
+        if (configEncoder == null) {
+            throw new IllegalArgumentException("Config encoder cannot be null");
+        }
+        this.configEncoder = configEncoder;
+        this.file = file;
+    }
+
+    @Override
+    public void write(T value) throws Exception {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public void close() throws Exception {
+        // Nothing
+    }
+}
