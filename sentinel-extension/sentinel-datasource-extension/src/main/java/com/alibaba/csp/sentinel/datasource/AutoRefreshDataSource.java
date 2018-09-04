174930f1471cf8a2490c83515882330c3dfc34b0
yfh0918 <yfh0918@sina.cn>
2018-09-04 22:21:20 +0800

    代码Formatter

--- sentinel-extension/sentinel-datasource-extension/src/main/java/com/alibaba/csp/sentinel/datasource/AutoRefreshDataSource.java
+++ sentinel-extension/sentinel-datasource-extension/src/main/java/com/alibaba/csp/sentinel/datasource/AutoRefreshDataSource.java
@@ -25,57 +25,59 @@ import com.alibaba.csp.sentinel.log.RecordLog;
 /**
  * A {@link ReadableDataSource} automatically fetches the backend data.
  *
- * @param <S> source data type
- * @param <T> target data type
+ * @param <S>
+ *            source data type
+ * @param <T>
+ *            target data type
  * @author Carpenter Lee
  */
 public abstract class AutoRefreshDataSource<S, T> extends AbstractDataSource<S, T> {
 
-    private ScheduledExecutorService service;
-    protected long recommendRefreshMs = 3000;
+	private ScheduledExecutorService service;
+	protected long recommendRefreshMs = 3000;
 
-    public AutoRefreshDataSource(Converter<S, T> configParser) {
-        super(configParser);
-        startTimerService();
-    }
+	public AutoRefreshDataSource(Converter<S, T> configParser) {
+		super(configParser);
+		startTimerService();
+	}
 
-    public AutoRefreshDataSource(Converter<S, T> configParser, final long recommendRefreshMs) {
-        super(configParser);
-        if (recommendRefreshMs <= 0) {
-            throw new IllegalArgumentException("recommendRefreshMs must > 0, but " + recommendRefreshMs + " get");
-        }
-        this.recommendRefreshMs = recommendRefreshMs;
-        startTimerService();
-    }
+	public AutoRefreshDataSource(Converter<S, T> configParser, final long recommendRefreshMs) {
+		super(configParser);
+		if (recommendRefreshMs <= 0) {
+			throw new IllegalArgumentException("recommendRefreshMs must > 0, but " + recommendRefreshMs + " get");
+		}
+		this.recommendRefreshMs = recommendRefreshMs;
+		startTimerService();
+	}
 
-    private void startTimerService() {
-        service = Executors.newScheduledThreadPool(1,
-            new NamedThreadFactory("sentinel-datasource-auto-refresh-task", true));
-        service.scheduleAtFixedRate(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                	if (!refresh()) {
-                		return;
-                	}
-                    T newValue = loadConfig();
-                    getProperty().updateValue(newValue);
-                } catch (Throwable e) {
-                    RecordLog.info("loadConfig exception", e);
-                }
-            }
-        }, recommendRefreshMs, recommendRefreshMs, TimeUnit.MILLISECONDS);
-    }
+	private void startTimerService() {
+		service = Executors.newScheduledThreadPool(1,
+				new NamedThreadFactory("sentinel-datasource-auto-refresh-task", true));
+		service.scheduleAtFixedRate(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					if (!refresh()) {
+						return;
+					}
+					T newValue = loadConfig();
+					getProperty().updateValue(newValue);
+				} catch (Throwable e) {
+					RecordLog.info("loadConfig exception", e);
+				}
+			}
+		}, recommendRefreshMs, recommendRefreshMs, TimeUnit.MILLISECONDS);
+	}
 
-    @Override
-    public void close() throws Exception {
-        if (service != null) {
-            service.shutdownNow();
-            service = null;
-        }
-    }
-    
-    protected boolean refresh(){
-    	return true;
-    }
+	@Override
+	public void close() throws Exception {
+		if (service != null) {
+			service.shutdownNow();
+			service = null;
+		}
+	}
+
+	protected boolean refresh() {
+		return true;
+	}
 }
--- sentinel-extension/sentinel-datasource-extension/src/main/java/com/alibaba/csp/sentinel/datasource/FileWritableDataSource.java
+++ sentinel-extension/sentinel-datasource-extension/src/main/java/com/alibaba/csp/sentinel/datasource/FileWritableDataSource.java
@@ -21,57 +21,58 @@ import java.io.FileOutputStream;
 /**
  * A {@link WritableDataSource} based on file.
  *
- * @param <T> data type
+ * @param <T>
+ *            data type
  * @author Eric Zhao
  * @since 0.2.0
  */
 public class FileWritableDataSource<T> implements WritableDataSource<T> {
 
-    private final Converter<T, String> configEncoder;
-    private File file;
+	private final Converter<T, String> configEncoder;
+	private File file;
 
-    public FileWritableDataSource(String filePath, Converter<T, String> configEncoder) {
-        this(new File(filePath), configEncoder);
-    }
+	public FileWritableDataSource(String filePath, Converter<T, String> configEncoder) {
+		this(new File(filePath), configEncoder);
+	}
 
-    public FileWritableDataSource(File file, Converter<T, String> configEncoder) {
-        if (file == null || file.isDirectory()) {
-            throw new IllegalArgumentException("Bad file");
-        }
-        if (configEncoder == null) {
-            throw new IllegalArgumentException("Config encoder cannot be null");
-        }
-        this.configEncoder = configEncoder;
-        this.file = file;
-    }
+	public FileWritableDataSource(File file, Converter<T, String> configEncoder) {
+		if (file == null || file.isDirectory()) {
+			throw new IllegalArgumentException("Bad file");
+		}
+		if (configEncoder == null) {
+			throw new IllegalArgumentException("Config encoder cannot be null");
+		}
+		this.configEncoder = configEncoder;
+		this.file = file;
+	}
 
-    @Override
-    public void write(T value) throws Exception {
-    	if (configEncoder == null) {
+	@Override
+	public void write(T value) throws Exception {
+		if (configEncoder == null) {
 			throw new NullPointerException("configEncoder is null Can't write");
 		}
-		synchronized(file) {
+		synchronized (file) {
 			String convertResult = configEncoder.convert(value);
-    		FileOutputStream outputStream = null;
-            try {
-                outputStream = new FileOutputStream(file);
-                byte[] bytesArray = convertResult.getBytes();
-                outputStream.write(bytesArray);
-                outputStream.flush();
-            } finally {
-                if (outputStream != null) {
-                    try {
-                    	outputStream.close();
-                    } catch (Exception ignore) {
-                    	//nothing
-                    }
-                }
-            }
-    	}
-    }
+			FileOutputStream outputStream = null;
+			try {
+				outputStream = new FileOutputStream(file);
+				byte[] bytesArray = convertResult.getBytes();
+				outputStream.write(bytesArray);
+				outputStream.flush();
+			} finally {
+				if (outputStream != null) {
+					try {
+						outputStream.close();
+					} catch (Exception ignore) {
+						// nothing
+					}
+				}
+			}
+		}
+	}
 
-    @Override
-    public void close() throws Exception {
-        // Nothing
-    }
+	@Override
+	public void close() throws Exception {
+		// Nothing
+	}
 }
