d73bd91945d6c7fe7453da9f1bb2400508834f89
Eric Zhao <sczyh16@gmail.com>
2018-09-12 13:53:43 +0800

    Support extensible slot chain builder using SPI mechanism (#145)
    - Support extensible `SlotChainBuilder` using SPI mechanism
    - Add a `SlotChainProvider` to load slot chain builder and create new slot chains
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/Env.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/Env.java
@@ -18,15 +18,12 @@ package com.alibaba.csp.sentinel;
 import com.alibaba.csp.sentinel.init.InitExecutor;
 import com.alibaba.csp.sentinel.node.DefaultNodeBuilder;
 import com.alibaba.csp.sentinel.node.NodeBuilder;
-import com.alibaba.csp.sentinel.slots.DefaultSlotsChainBuilder;
-import com.alibaba.csp.sentinel.slots.SlotsChainBuilder;
 
 /**
  * @author jialiang.linjl
  */
 public class Env {
 
-    public static final SlotsChainBuilder slotsChainbuilder = new DefaultSlotsChainBuilder();
     public static final NodeBuilder nodeBuilder = new DefaultNodeBuilder();
     public static final Sph sph = new CtSph();
 
