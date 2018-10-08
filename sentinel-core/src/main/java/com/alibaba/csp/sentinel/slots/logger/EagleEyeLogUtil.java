85540229e936abbd1481bc0c28f5bdcc958078ab
Carpenter Lee <hooleeucas@163.com>
2018-10-08 19:46:38 +0800

    Change eagleEye log path when log base configured.

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/logger/EagleEyeLogUtil.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/logger/EagleEyeLogUtil.java
@@ -15,28 +15,26 @@
  */
 package com.alibaba.csp.sentinel.slots.logger;
 
-import java.io.File;
-
 import com.alibaba.csp.sentinel.eagleeye.EagleEye;
 import com.alibaba.csp.sentinel.eagleeye.StatLogger;
+import com.alibaba.csp.sentinel.log.LogBase;
 
 public class EagleEyeLogUtil {
 
-    private static final String DIR_NAME = "csp";
-    private static final String FILE_NAME = "sentinel-block.log";
+    public static final String FILE_NAME = "sentinel-block.log";
 
     private static StatLogger statLogger;
 
     static {
-        String path = DIR_NAME + File.separator + FILE_NAME;
+        String path = LogBase.getLogBaseDir() + FILE_NAME;
 
-        statLogger = EagleEye.statLoggerBuilder("sentinel-block-record")
+        statLogger = EagleEye.statLoggerBuilder("sentinel-block-log")
             .intervalSeconds(1)
             .entryDelimiter('|')
             .keyDelimiter(',')
             .valueDelimiter(',')
             .maxEntryCount(6000)
-            .baseLogFilePath(path)
+            .configLogFilePath(path)
             .maxFileSizeMB(300)
             .maxBackupIndex(3)
             .buildSingleton();
