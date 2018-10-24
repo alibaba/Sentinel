be29fd7939fd2095cd55c9be1e694c499c35e94c
Eric Zhao <sczyh16@gmail.com>
2018-10-24 14:46:42 +0800

    Refactor logic of getting resource name in Sentinel annotation
    - Enhance resolve name logic in MethodUtil
    - Add test case for MethodUtil
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-extension/sentinel-annotation-aspectj/src/main/java/com/alibaba/csp/sentinel/annotation/aspectj/SentinelResourceAspect.java
+++ sentinel-extension/sentinel-annotation-aspectj/src/main/java/com/alibaba/csp/sentinel/annotation/aspectj/SentinelResourceAspect.java
@@ -26,6 +26,7 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.context.ContextUtil;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.csp.sentinel.slots.block.degrade.DegradeException;
+import com.alibaba.csp.sentinel.util.MethodUtil;
 import com.alibaba.csp.sentinel.util.StringUtil;
 
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -76,28 +77,16 @@ public class SentinelResourceAspect {
             ContextUtil.exit();
         }
     }
-    
+
     private String getResourceName(String resourceName, Method method) {
-        if(StringUtil.isNotBlank(resourceName)){
+        // If resource name is present in annotation, use this value.
+        if (StringUtil.isNotBlank(resourceName)) {
             return resourceName;
         }
-        StringBuilder buf = new StringBuilder(64);
-        buf.append(method.getDeclaringClass().getName())
-            .append(":")
-            .append(method.getName())
-            .append("(");
-        boolean isFirst = true;
-        for (Class<?> clazz : method.getParameterTypes()) {
-            if (!isFirst) {
-                buf.append(",");
-            }
-            buf.append(clazz.getName());
-            isFirst = false;
-        }
-        buf.append(")");
-        return buf.toString();
+        // Parse name of target method.
+        return MethodUtil.resolveMethodName(method);
     }
-    
+
     private Object handleBlockException(ProceedingJoinPoint pjp, SentinelResource annotation, BlockException ex)
         throws Exception {
         // Execute fallback for degrading if configured.
@@ -224,7 +213,8 @@ public class SentinelResourceAspect {
         MethodSignature signature = (MethodSignature)joinPoint.getSignature();
         Class<?> targetClass = joinPoint.getTarget().getClass();
 
-        Method method = getDeclaredMethodFor(targetClass, signature.getName(), signature.getMethod().getParameterTypes());
+        Method method = getDeclaredMethodFor(targetClass, signature.getName(),
+            signature.getMethod().getParameterTypes());
         if (method == null) {
             throw new IllegalStateException("Cannot resolve target method: " + signature.getMethod().getName());
         }
@@ -235,8 +225,8 @@ public class SentinelResourceAspect {
      * Get declared method with provided name and parameterTypes in given class and its super classes.
      * All parameters should be valid.
      *
-     * @param clazz class where the method is located
-     * @param name method name
+     * @param clazz          class where the method is located
+     * @param name           method name
      * @param parameterTypes method parameter type list
      * @return resolved method, null if not found
      */
