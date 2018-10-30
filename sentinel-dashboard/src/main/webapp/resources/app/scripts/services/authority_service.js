c25fa0292f1d6143c7c4ec7bf27b8add9faf3fe8
Eric Zhao <sczyh16@gmail.com>
2018-10-30 11:41:05 +0800

    Add support for adding param rule / authority rule in resource page of dashboard
    - Decouple rule checking from controller (now in service)
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-dashboard/src/main/webapp/resources/app/scripts/services/authority_service.js
+++ sentinel-dashboard/src/main/webapp/resources/app/scripts/services/authority_service.js
@@ -37,4 +37,20 @@ angular.module('sentinelDashboardApp').service('AuthorityRuleService', ['$http',
             method: 'DELETE'
         });
     };
+
+    this.checkRuleValid = function checkRuleValid(rule) {
+        if (rule.resource === undefined || rule.resource === '') {
+            alert('资源名称不能为空');
+            return false;
+        }
+        if (rule.limitApp === undefined || rule.limitApp === '') {
+            alert('流控针对应用不能为空');
+            return false;
+        }
+        if (rule.strategy === undefined) {
+            alert('必须选择黑白名单模式');
+            return false;
+        }
+        return true;
+    };
 }]);
