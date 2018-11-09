aaea13e0c9890e2b8d729c2368c0e570eef67e19
Eric Zhao <sczyh16@gmail.com>
2018-11-09 11:08:56 +0800

    Extract flow rule checker from FlowRule
    - Extract flow rule checker from legacy `passCheck`
    - Remove redundant code
    - Refactor FlowSlot (some logic moved from FlowRuleManager)
    - Rename `Controller` to `TrafficShapingController`
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/TrafficShapingController.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/TrafficShapingController.java
@@ -18,10 +18,18 @@ package com.alibaba.csp.sentinel.slots.block.flow;
 import com.alibaba.csp.sentinel.node.Node;
 
 /**
+ * A universal interface for traffic shaping controller.
+ *
  * @author jialiang.linjl
  */
-public interface Controller {
+public interface TrafficShapingController {
 
+    /**
+     * Check whether given resource entry can pass with provided count.
+     *
+     * @param node resource node
+     * @param acquireCount count to acquire
+     * @return true if the resource entry can pass; false if it should be blocked
+     */
     boolean canPass(Node node, int acquireCount);
-
 }
