a31e79878db392451df106a01f5b220c1f597d82
Eric Zhao <sczyh16@gmail.com>
2018-11-21 19:02:21 +0800

    Update parameter flow rule to adapt to cluster mode and extract rule util class
    - Update ParamFlowRule to support cluster mode
    - Add `ParamFlowClusterConfig` to provide cluster mode items for the rule
    - Update ParamFlowChecker to support cluster flow mode
    - Extract ParamFlowRuleUtil class
    - Change type of `flowId` from Integer to Long
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-extension/sentinel-parameter-flow-control/src/test/java/com/alibaba/csp/sentinel/slots/block/flow/param/ParamFlowRuleManagerTest.java
+++ sentinel-extension/sentinel-parameter-flow-control/src/test/java/com/alibaba/csp/sentinel/slots/block/flow/param/ParamFlowRuleManagerTest.java
@@ -15,11 +15,9 @@
  */
 package com.alibaba.csp.sentinel.slots.block.flow.param;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import com.alibaba.csp.sentinel.EntryType;
 import com.alibaba.csp.sentinel.slotchain.StringResourceWrapper;
@@ -108,85 +106,5 @@ public class ParamFlowRuleManagerTest {
         assertTrue(allRules.contains(ruleB));
         assertTrue(allRules.contains(ruleC));
         assertTrue(allRules.contains(ruleD));
-    }
-
-    @Test
-    public void testParseHotParamExceptionItemsFailure() {
-        String valueB = "Sentinel";
-        Integer valueC = 6;
-        char valueD = 6;
-        float valueE = 11.11f;
-        // Null object will not be parsed.
-        ParamFlowItem itemA = new ParamFlowItem(null, 1, double.class.getName());
-        // Hot item with empty class type will be treated as string.
-        ParamFlowItem itemB = new ParamFlowItem(valueB, 3, null);
-        ParamFlowItem itemE = new ParamFlowItem(String.valueOf(valueE), 3, "");
-        // Bad count will not be parsed.
-        ParamFlowItem itemC = ParamFlowItem.newItem(valueC, -5);
-        ParamFlowItem itemD = new ParamFlowItem(String.valueOf(valueD), null, char.class.getName());
-
-        List<ParamFlowItem> badItems = Arrays.asList(itemA, itemB, itemC, itemD, itemE);
-        Map<Object, Integer> parsedItems = ParamFlowRuleManager.parseHotItems(badItems);
-
-        // Value B and E will be parsed, but ignoring the type.
-        assertEquals(2, parsedItems.size());
-        assertEquals(itemB.getCount(), parsedItems.get(valueB));
-        assertFalse(parsedItems.containsKey(valueE));
-        assertEquals(itemE.getCount(), parsedItems.get(String.valueOf(valueE)));
-    }
-
-    @Test
-    public void testParseHotParamExceptionItemsSuccess() {
-        // Test for empty list.
-        assertEquals(0, ParamFlowRuleManager.parseHotItems(null).size());
-        assertEquals(0, ParamFlowRuleManager.parseHotItems(new ArrayList<ParamFlowItem>()).size());
-
-        // Test for boxing objects and primitive types.
-        Double valueA = 1.1d;
-        String valueB = "Sentinel";
-        Integer valueC = 6;
-        char valueD = 'c';
-        ParamFlowItem itemA = ParamFlowItem.newItem(valueA, 1);
-        ParamFlowItem itemB = ParamFlowItem.newItem(valueB, 3);
-        ParamFlowItem itemC = ParamFlowItem.newItem(valueC, 5);
-        ParamFlowItem itemD = new ParamFlowItem().setObject(String.valueOf(valueD))
-            .setClassType(char.class.getName())
-            .setCount(7);
-        List<ParamFlowItem> items = Arrays.asList(itemA, itemB, itemC, itemD);
-        Map<Object, Integer> parsedItems = ParamFlowRuleManager.parseHotItems(items);
-        assertEquals(itemA.getCount(), parsedItems.get(valueA));
-        assertEquals(itemB.getCount(), parsedItems.get(valueB));
-        assertEquals(itemC.getCount(), parsedItems.get(valueC));
-        assertEquals(itemD.getCount(), parsedItems.get(valueD));
-    }
-
-    @Test
-    public void testCheckValidHotParamRule() {
-        // Null or empty resource;
-        ParamFlowRule rule1 = new ParamFlowRule();
-        ParamFlowRule rule2 = new ParamFlowRule("");
-        assertFalse(ParamFlowRuleManager.isValidRule(null));
-        assertFalse(ParamFlowRuleManager.isValidRule(rule1));
-        assertFalse(ParamFlowRuleManager.isValidRule(rule2));
-
-        // Invalid threshold count.
-        ParamFlowRule rule3 = new ParamFlowRule("abc")
-            .setCount(-1)
-            .setParamIdx(1);
-        assertFalse(ParamFlowRuleManager.isValidRule(rule3));
-
-        // Parameter index not set or invalid.
-        ParamFlowRule rule4 = new ParamFlowRule("abc")
-            .setCount(1);
-        ParamFlowRule rule5 = new ParamFlowRule("abc")
-            .setCount(1)
-            .setParamIdx(-1);
-        assertFalse(ParamFlowRuleManager.isValidRule(rule4));
-        assertFalse(ParamFlowRuleManager.isValidRule(rule5));
-
-        ParamFlowRule goodRule = new ParamFlowRule("abc")
-            .setCount(10)
-            .setParamIdx(1);
-        assertTrue(ParamFlowRuleManager.isValidRule(goodRule));
     }
 }
