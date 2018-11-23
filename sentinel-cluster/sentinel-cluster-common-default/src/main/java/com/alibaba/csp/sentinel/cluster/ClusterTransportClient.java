b3ae7f58bf8c1878c9fceee3caf965bba59d8895
Eric Zhao <sczyh16@gmail.com>
2018-11-23 17:53:36 +0800

    Add common module for Sentinel default cluster implementation
    - Add a `ClusterTransportClient` for transport abstraction of Sentinel cluster
    - Add universal request/response interface and common ClusterRequest/ClusterResponse abstraction
    - Add common request/response data entity
    - Add basic abstraction of codec (EntityWriter and EntityDecoder)
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

---
+++ sentinel-cluster/sentinel-cluster-common-default/src/main/java/com/alibaba/csp/sentinel/cluster/ClusterTransportClient.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.csp.sentinel.cluster;
+
+import com.alibaba.csp.sentinel.cluster.request.ClusterRequest;
+import com.alibaba.csp.sentinel.cluster.response.ClusterResponse;
+
+/**
+ * Synchronous transport client for distributed flow control.
+ *
+ * @author Eric Zhao
+ * @since 1.4.0
+ */
+public interface ClusterTransportClient {
+
+    /**
+     * Send request to remote server and get response.
+     *
+     * @param request Sentinel cluster request
+     * @return response from remote server
+     * @throws Exception some error occurs
+     */
+    ClusterResponse sendRequest(ClusterRequest request) throws Exception;
+}
