1b68d0c9a0cd2b70008555d0222a697445ee911a
Eric Zhao <sczyh16@gmail.com>
2019-01-04 14:36:46 +0800

    Polish cluster token server handler related logic
    - Enlarge retry timeout to 2s by default
    - Improve remote address representation
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-cluster/sentinel-cluster-server-default/src/main/java/com/alibaba/csp/sentinel/cluster/server/config/ServerTransportConfig.java
+++ sentinel-cluster/sentinel-cluster-server-default/src/main/java/com/alibaba/csp/sentinel/cluster/server/config/ServerTransportConfig.java
@@ -15,20 +15,21 @@
  */
 package com.alibaba.csp.sentinel.cluster.server.config;
 
+import com.alibaba.csp.sentinel.cluster.ClusterConstants;
+
 /**
  * @author Eric Zhao
  * @since 1.4.0
  */
 public class ServerTransportConfig {
 
-    public static final int DEFAULT_PORT = 8730;
     public static final int DEFAULT_IDLE_SECONDS = 600;
 
     private int port;
     private int idleSeconds;
 
     public ServerTransportConfig() {
-        this(DEFAULT_PORT, DEFAULT_IDLE_SECONDS);
+        this(ClusterConstants.DEFAULT_CLUSTER_SERVER_PORT, DEFAULT_IDLE_SECONDS);
     }
 
     public ServerTransportConfig(int port, int idleSeconds) {
