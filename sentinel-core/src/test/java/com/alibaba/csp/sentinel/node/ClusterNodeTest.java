a771293d6b0288f1258f8c496af44a20f2e79f33
cdfive <176402936@qq.com>
2019-01-16 00:00:29 +0800

    synchronized add operation of hashset and repeat the test method of multiThread

--- sentinel-core/src/test/java/com/alibaba/csp/sentinel/node/ClusterNodeTest.java
+++ sentinel-core/src/test/java/com/alibaba/csp/sentinel/node/ClusterNodeTest.java
@@ -64,51 +64,61 @@ public class ClusterNodeTest {
 
     @Test
     public void testGetOrCreateOriginNodeMultiThread() {
-        final ClusterNode clusterNode = new ClusterNode();
+        // note: in junit4, repeat execute a test method is not very convenient
+        // for simple, here use a loop instead
+        // https://stackoverflow.com/questions/1492856/easy-way-of-running-the-same-junit-test-over-and-over
+        // in junit5, use @RepeatedTest(10)
+        int testTimes = 10;// execute 10 times, test will have chance to failed, if remove the lock in ClusterNode
+        for (int times = 0; times < testTimes; times++) {
+            final ClusterNode clusterNode = new ClusterNode();
 
-        // store all distinct nodes by calling ClusterNode#getOrCreateOriginNode
-        final Set<Node> createdNodes = new HashSet<Node>();
+            // store all distinct nodes by calling ClusterNode#getOrCreateOriginNode
+            final Set<Node> createdNodes = new HashSet<Node>();
 
-        final Random random = new Random();
+            final Random random = new Random();
 
-        // 10 threads, 3 origins, 20 tasks(in total, calling 20 times of ClusterNode#getOrCreateOriginNode concurrently)
-        final ExecutorService es = Executors.newFixedThreadPool(10);
-        final List<String> origins = Arrays.asList("origin1", "origin2", "origin3");
-        int taskCount = 20;
+            // 10 threads, 3 origins, 20 tasks(in total, calling 20 times of ClusterNode#getOrCreateOriginNode concurrently)
+            final ExecutorService es = Executors.newFixedThreadPool(10);
+            final List<String> origins = Arrays.asList("origin1", "origin2", "origin3");
+            int taskCount = 20;
 
-        List<Callable<Object>> tasks = new ArrayList<Callable<Object>>(taskCount);
-        for (int i = 0; i < taskCount; i++) {
-            tasks.add(new Callable<Object>() {
-                @Override
-                public Object call() throws Exception {
-                    // one task call one times of ClusterNode#getOrCreateOriginNode
-                    Node node = clusterNode.getOrCreateOriginNode(origins.get(random.nextInt(origins.size())));
-                    // add the result node to the createdNodes set
-                    createdNodes.add(node);
-                    return null;
-                }
-            });
-        }
+            List<Callable<Object>> tasks = new ArrayList<Callable<Object>>(taskCount);
+            for (int i = 0; i < taskCount; i++) {
+                tasks.add(new Callable<Object>() {
+                    @Override
+                    public Object call() throws Exception {
+                        // one task call one times of ClusterNode#getOrCreateOriginNode
+                        Node node = clusterNode.getOrCreateOriginNode(origins.get(random.nextInt(origins.size())));
+                        // add the result node to the createdNodes set
+                        // node: since HashSet is non-threadsafe, synchronized the ClusterNodeTest.class
+                        synchronized (ClusterNodeTest.class) {
+                            createdNodes.add(node);
+                        }
+                        return null;
+                    }
+                });
+            }
 
-        try {
-            es.invokeAll(tasks);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        es.shutdown();
+            try {
+                es.invokeAll(tasks);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            es.shutdown();
 
-        // origins.size() origins, the same count as the originCountMap
-        assertEquals(origins.size(), clusterNode.getOriginCountMap().size());
+            // origins.size() origins, the same count as the originCountMap
+            assertEquals(origins.size(), clusterNode.getOriginCountMap().size());
 
-        // not use `assertEquals(origins.size(), createdNodes.size());`, but a compare judgement for debug
-        if (origins.size() != createdNodes.size()) {
-            // for debug, we can add a breakpoint here to see the detail info of createdNodes, if remove the lock in ClusterNode
-            fail("originCountMap's size should " + origins.size() + ", but actual " + createdNodes.size());
-        }
+            // not use `assertEquals(origins.size(), createdNodes.size());`, but a compare judgement for debug
+            if (origins.size() != createdNodes.size()) {
+                // for debug, we can add a breakpoint here to see the detail info of createdNodes, if remove the lock in ClusterNode
+                fail("originCountMap's size should " + origins.size() + ", but actual " + createdNodes.size());
+            }
 
-        // verify originCountMap's key
-        for (String origin : origins) {
-            assertTrue(clusterNode.getOriginCountMap().containsKey(origin));
+            // verify originCountMap's key
+            for (String origin : origins) {
+                assertTrue(clusterNode.getOriginCountMap().containsKey(origin));
+            }
         }
     }
 
