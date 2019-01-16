aacb4d6de1db98537da53b4a2ce0218d10b5424a
Carpenter Lee <hooleeucas@163.com>
2019-01-16 15:21:16 +0800

    Bug fix: fix 420, fix negative waitTime in RateLimiterController and WarmUpRateLimiterController
    Signed-off-by: Carpenter Lee <hooleeucas@163.com>

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/controller/RateLimiterController.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/flow/controller/RateLimiterController.java
@@ -58,17 +58,20 @@ public class RateLimiterController implements TrafficShapingController {
         } else {
             // Calculate the time to wait.
             long waitTime = costTime + latestPassedTime.get() - TimeUtil.currentTimeMillis();
-            if (waitTime >= maxQueueingTimeMs) {
+            if (waitTime > maxQueueingTimeMs) {
                 return false;
             } else {
                 long oldTime = latestPassedTime.addAndGet(costTime);
                 try {
                     waitTime = oldTime - TimeUtil.currentTimeMillis();
-                    if (waitTime >= maxQueueingTimeMs) {
+                    if (waitTime > maxQueueingTimeMs) {
                         latestPassedTime.addAndGet(-costTime);
                         return false;
                     }
-                    Thread.sleep(waitTime);
+                    // in race condition waitTime may <= 0
+                    if (waitTime > 0) {
+                        Thread.sleep(waitTime);
+                    }
                     return true;
                 } catch (InterruptedException e) {
                 }
