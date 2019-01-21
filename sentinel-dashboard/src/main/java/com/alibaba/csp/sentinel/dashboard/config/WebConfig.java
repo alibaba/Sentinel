1f8236a9e86dc35330dc015962cbba55db34c5f4
Eric Zhao <sczyh16@gmail.com>
2019-01-21 19:18:20 +0800

    dashboard: update WebConfig and improve token server list page
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/config/WebConfig.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/config/WebConfig.java
@@ -26,13 +26,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author leyou
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
     private final Logger logger = LoggerFactory.getLogger(WebConfig.class);
 
@@ -62,5 +62,4 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
         return registration;
     }
-
 }
