748942bd40c10b18194548a16b6331d2c4476cc1
minjun <mjaow0128@gmail.com>
2019-01-31 15:22:00 +0800

    clean code

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/RuleConstant.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/RuleConstant.java
@@ -54,6 +54,5 @@ public final class RuleConstant {
     public static final int DEFAULT_SAMPLE_COUNT = 2;
     public static final int DEFAULT_WINDOW_INTERVAL_MS = 1000;
 
-    private RuleConstant() {
-    }
+    private RuleConstant() {}
 }
