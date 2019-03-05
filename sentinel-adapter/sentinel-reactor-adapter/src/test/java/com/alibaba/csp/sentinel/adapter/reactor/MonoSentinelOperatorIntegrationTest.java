c39a0c77b6eb886d9493367f25b5049b56a1668e
Eric Zhao <sczyh16@gmail.com>
2019-03-05 20:27:13 +0800

    Add basic test cases for reactor adapter
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

---
+++ sentinel-adapter/sentinel-reactor-adapter/src/test/java/com/alibaba/csp/sentinel/adapter/reactor/MonoSentinelOperatorIntegrationTest.java
@@ -0,0 +1,135 @@
+package com.alibaba.csp.sentinel.adapter.reactor;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.alibaba.csp.sentinel.node.ClusterNode;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.csp.sentinel.slots.clusterbuilder.ClusterBuilderSlot;
+
+import org.junit.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Eric Zhao
+ */
+public class MonoSentinelOperatorIntegrationTest {
+
+    @Test
+    public void testFluxToMonoNextThenCancelSuccess() {
+        String resourceName = createResourceName("testFluxToMonoNextThenCancelSuccess");
+        StepVerifier.create(Flux.range(1, 10)
+            .map(e -> e * 2)
+            .next()
+            .transform(new SentinelReactorTransformer<>(resourceName)))
+            .expectNext(2)
+            .verifyComplete();
+
+        ClusterNode cn = ClusterBuilderSlot.getClusterNode(resourceName);
+        assertNotNull(cn);
+        assertEquals(1, cn.passQps(), 0.01);
+    }
+
+    @Test
+    public void testEmitSingleLongTimeRt() {
+        String resourceName = createResourceName("testEmitSingleLongTimeRt");
+        StepVerifier.create(Mono.just(2)
+            .delayElement(Duration.ofMillis(1000))
+            .map(e -> e * 2)
+            .transform(new SentinelReactorTransformer<>(resourceName)))
+            .expectNext(4)
+            .verifyComplete();
+
+        ClusterNode cn = ClusterBuilderSlot.getClusterNode(resourceName);
+        assertNotNull(cn);
+        assertEquals(1000, cn.avgRt(), 20);
+    }
+
+    @Test
+    public void testEmitEmptySuccess() {
+        String resourceName = createResourceName("testEmitEmptySuccess");
+        StepVerifier.create(Mono.empty()
+            .transform(new SentinelReactorTransformer<>(resourceName)))
+            .verifyComplete();
+
+        ClusterNode cn = ClusterBuilderSlot.getClusterNode(resourceName);
+        assertNotNull(cn);
+        assertEquals(1, cn.passQps(), 0.01);
+    }
+
+    @Test
+    public void testEmitSingleSuccess() {
+        String resourceName = createResourceName("testEmitSingleSuccess");
+        StepVerifier.create(Mono.just(1)
+            .transform(new SentinelReactorTransformer<>(resourceName)))
+            .expectNext(1)
+            .verifyComplete();
+
+        ClusterNode cn = ClusterBuilderSlot.getClusterNode(resourceName);
+        assertNotNull(cn);
+        assertEquals(1, cn.passQps(), 0.01);
+    }
+
+    @Test
+    public void testEmitSingleValueWhenFlowControlTriggered() {
+        String resourceName = createResourceName("testEmitSingleValueWhenFlowControlTriggered");
+        FlowRuleManager.loadRules(Collections.singletonList(
+            new FlowRule(resourceName).setCount(0)
+        ));
+        StepVerifier.create(Mono.just(1)
+            .map(e -> e * 2)
+            .transform(new SentinelReactorTransformer<>(resourceName)))
+            .expectError(BlockException.class)
+            .verify();
+
+        ClusterNode cn = ClusterBuilderSlot.getClusterNode(resourceName);
+        assertNotNull(cn);
+        assertEquals(0, cn.passQps(), 0.01);
+        assertEquals(1, cn.blockRequest());
+
+        FlowRuleManager.loadRules(new ArrayList<>());
+    }
+
+    @Test
+    public void testEmitExceptionWhenFlowControlTriggered() {
+        String resourceName = createResourceName("testEmitExceptionWhenFlowControlTriggered");
+        FlowRuleManager.loadRules(Collections.singletonList(
+            new FlowRule(resourceName).setCount(0)
+        ));
+        StepVerifier.create(Mono.error(new IllegalStateException("some"))
+            .transform(new SentinelReactorTransformer<>(resourceName)))
+            .expectError(BlockException.class)
+            .verify();
+
+        ClusterNode cn = ClusterBuilderSlot.getClusterNode(resourceName);
+        assertNotNull(cn);
+        assertEquals(0, cn.passQps(), 0.01);
+        assertEquals(1, cn.blockRequest());
+
+        FlowRuleManager.loadRules(new ArrayList<>());
+    }
+
+    @Test
+    public void testEmitSingleError() {
+        String resourceName = createResourceName("testEmitSingleError");
+        StepVerifier.create(Mono.error(new IllegalStateException())
+            .transform(new SentinelReactorTransformer<>(resourceName)))
+            .expectError(IllegalStateException.class)
+            .verify();
+
+        ClusterNode cn = ClusterBuilderSlot.getClusterNode(resourceName);
+        assertNotNull(cn);
+        assertEquals(1, cn.totalException());
+    }
+
+    private String createResourceName(String resourceName) {
+        return "reactor_test_mono_" + resourceName;
+    }
+}
