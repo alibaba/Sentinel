ae243801e500e2620d0edc88082eecafd5f284d5
cdfive <31885791+cdfive@users.noreply.github.com>
2019-03-16 12:32:35 +0800

    Add some unit test for sentinel-dubbo-adapter module (#569)

--- sentinel-adapter/sentinel-dubbo-adapter/src/test/java/com/alibaba/csp/sentinel/adapter/dubbo/provider/impl/DemoServiceImpl.java
+++ sentinel-adapter/sentinel-dubbo-adapter/src/test/java/com/alibaba/csp/sentinel/adapter/dubbo/provider/impl/DemoServiceImpl.java
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.csp.sentinel.adapter.dubbo.provider;
+package com.alibaba.csp.sentinel.adapter.dubbo.provider.impl;
 
-import com.alibaba.csp.sentinel.adapter.dubbo.DemoService;
+import com.alibaba.csp.sentinel.adapter.dubbo.provider.DemoService;
 
 /**
  * @author leyou
