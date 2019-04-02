e792643acf94496fa02521f55623fed7c1a75b35
Eric Zhao <sczyh16@gmail.com>
2019-04-02 10:38:57 +0800

    Support passing parameters via SentinelReactorTransformer
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-adapter/sentinel-reactor-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/reactor/MonoSentinelOperator.java
+++ sentinel-adapter/sentinel-reactor-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/reactor/MonoSentinelOperator.java
@@ -15,6 +15,8 @@
  */
 package com.alibaba.csp.sentinel.adapter.reactor;
 
+import com.alibaba.csp.sentinel.util.AssertUtil;
+
 import reactor.core.CoreSubscriber;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.MonoOperator;
@@ -29,7 +31,7 @@ public class MonoSentinelOperator<T> extends MonoOperator<T, T> {
 
     public MonoSentinelOperator(Mono<? extends T> source, EntryConfig entryConfig) {
         super(source);
-        EntryConfig.assertValid(entryConfig);
+        AssertUtil.notNull(entryConfig, "entryConfig cannot be null");
         this.entryConfig = entryConfig;
     }
 
