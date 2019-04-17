78b4f42611a3951158869fc9e2c63d83c1a6943f
Eric Zhao <sczyh16@gmail.com>
2019-04-17 14:25:44 +0800

    Make CommandCenterProvider and HeartBeatSenderProvider choose the instance with highest precedence
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-transport/sentinel-transport-netty-http/src/main/java/com/alibaba/csp/sentinel/transport/heartbeat/HttpHeartbeatSender.java
+++ sentinel-transport/sentinel-transport-netty-http/src/main/java/com/alibaba/csp/sentinel/transport/heartbeat/HttpHeartbeatSender.java
@@ -19,6 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.alibaba.csp.sentinel.Constants;
+import com.alibaba.csp.sentinel.spi.SpiOrder;
 import com.alibaba.csp.sentinel.transport.config.TransportConfig;
 import com.alibaba.csp.sentinel.log.RecordLog;
 import com.alibaba.csp.sentinel.util.AppNameUtil;
@@ -39,6 +40,7 @@ import org.apache.http.impl.client.HttpClients;
  * @author Eric Zhao
  * @author leyou
  */
+@SpiOrder(SpiOrder.LOWEST_PRECEDENCE - 100)
 public class HttpHeartbeatSender implements HeartbeatSender {
 
     private final CloseableHttpClient client;
