02e9dfc017a345878b6c2ac0cd03796c78234727
yikangfeng <kangfeng01@126.com>
2019-04-18 14:27:11 +0800

    format code

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/annotation/SentinelResource.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/annotation/SentinelResource.java
@@ -1,14 +1,17 @@
 /*
  * Copyright 1999-2018 Alibaba Group Holding Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package com.alibaba.csp.sentinel.annotation;
 
@@ -43,9 +46,10 @@ public @interface SentinelResource {
     String blockHandler() default "";
 
     /**
-     * The {@code blockHandler} is located in the same class with the original method by default. However, if some
-     * methods share the same signature and intend to set the same block handler, then users can set the class where the
-     * block handler exists. Note that the block handler method must be static.
+     * The {@code blockHandler} is located in the same class with the original method by default.
+     * However, if some methods share the same signature and intend to set the same block handler,
+     * then users can set the class where the block handler exists. Note that the block handler method
+     * must be static.
      *
      * @return the class where the block handler exists, should not provide more than one classes
      */
@@ -61,7 +65,7 @@ public @interface SentinelResource {
      * @since 1.5.1
      */
     Class<? extends Throwable>[] exceptionsToTrace() default {Throwable.class};
-
+    
     /**
      * @return the list of exception classes to ignore, empty by default
      * @since 1.6.0
