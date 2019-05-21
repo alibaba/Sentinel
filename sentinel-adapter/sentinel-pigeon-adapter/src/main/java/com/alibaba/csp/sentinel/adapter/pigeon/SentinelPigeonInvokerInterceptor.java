45279cf9425b6a8cf3c04193603a851df2743271
wchswchs <wchswchs@gmail.com>
2019-05-21 14:15:51 +0800

    add pigeon adapter

--- sentinel-adapter/sentinel-pigeon-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/pigeon/SentinelPigeonInvokerInterceptor.java
+++ sentinel-adapter/sentinel-pigeon-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/pigeon/SentinelPigeonInvokerInterceptor.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 1999-2018 Alibaba Group Holding Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.alibaba.csp.sentinel.adapter.pigeon;
 
 import com.alibaba.csp.sentinel.Entry;
@@ -34,7 +19,7 @@ public class SentinelPigeonInvokerInterceptor implements InvokerInterceptor {
             String resourceName = PigeonUtils.getResourceName(invokerContext);
             methodEntry = SphU.entry(resourceName, EntryType.OUT);
         } catch (BlockException ex) {
-            PigeonFallbackRegistry.getInvokerFallback().handle(invokerContext, ex);
+            PigeonFallbackRegistry.getConsumerFallback().handle(invokerContext, ex);
         } catch (RpcException e) {
             Tracer.traceEntry(e, methodEntry);
             throw e;
