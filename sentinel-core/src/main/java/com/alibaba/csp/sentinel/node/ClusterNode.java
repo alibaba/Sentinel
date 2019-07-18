02507d215bbbb44fb4ea3ab0b0195ed0a735e748
yikangfeng <kangfeng01@126.com>
2019-07-18 17:18:57 +0800

    增加LRUCache

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/node/ClusterNode.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/node/ClusterNode.java
@@ -1,39 +1,35 @@
+package com.alibaba.csp.sentinel.node;
 /*
  * Copyright 1999-2018 Alibaba Group Holding Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
-package com.alibaba.csp.sentinel.node;
 
-import java.util.HashMap;
+
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
 
 import com.alibaba.csp.sentinel.context.ContextUtil;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.csp.sentinel.util.LRUCache;
 
 /**
  * <p>
- * This class stores summary runtime statistics of the resource, including rt, thread count, qps
- * and so on. Same resource shares the same {@link ClusterNode} globally, no matter in which
+ * This class stores summary runtime statistics of the resource, including rt, thread count, qps and so on. Same
+ * resource shares the same {@link ClusterNode} globally, no matter in which
  * {@link com.alibaba.csp.sentinel.context.Context}.
  * </p>
  * <p>
- * To distinguish invocation from different origin (declared in
- * {@link ContextUtil#enter(String name, String origin)}),
- * one {@link ClusterNode} holds an {@link #originCountMap}, this map holds {@link StatisticNode}
- * of different origin. Use {@link #getOrCreateOriginNode(String)} to get {@link Node} of the specific
- * origin.<br/>
+ * To distinguish invocation from different origin (declared in {@link ContextUtil#enter(String name, String origin)}),
+ * one {@link ClusterNode} holds an {@link #originCountMap}, this map holds {@link StatisticNode} of different origin.
+ * Use {@link #getOrCreateOriginNode(String)} to get {@link Node} of the specific origin.<br/>
  * Note that 'origin' usually is Service Consumer's app name.
  * </p>
  *
@@ -43,24 +39,30 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
 public class ClusterNode extends StatisticNode {
 
     /**
-     * <p>The origin map holds the pair: (origin, originNode) for one specific resource.</p>
      * <p>
-     * The longer the application runs, the more stable this mapping will become.
-     * So we didn't use concurrent map here, but a lock, as this lock only happens
-     * at the very beginning while concurrent map will hold the lock all the time.
+     * The origin map holds the pair: (origin, originNode) for one specific resource.
+     * </p>
+     * <p>
+     * The longer the application runs, the more stable this mapping will become. So we didn't use concurrent map here,
+     * but a lock, as this lock only happens at the very beginning while concurrent map will hold the lock all the time.
      * </p>
      */
-    private Map<String, StatisticNode> originCountMap = new HashMap<String, StatisticNode>();
+    private Map<String, StatisticNode> originCountMap = buildLRUCache();
 
     private final ReentrantLock lock = new ReentrantLock();
 
     /**
-     * <p>Get {@link Node} of the specific origin. Usually the origin is the Service Consumer's app name.</p>
-     * <p>If the origin node for given origin is absent, then a new {@link StatisticNode}
-     * for the origin will be created and returned.</p>
+     * <p>
+     * Get {@link Node} of the specific origin. Usually the origin is the Service Consumer's app name.
+     * </p>
+     * <p>
+     * If the origin node for given origin is absent, then a new {@link StatisticNode} for the origin will be created
+     * and returned.
+     * </p>
      *
-     * @param origin The caller's name, which is designated in the {@code parameter} parameter
-     *               {@link ContextUtil#enter(String name, String origin)}.
+     * @param origin
+     *            The caller's name, which is designated in the {@code parameter} parameter
+     *            {@link ContextUtil#enter(String name, String origin)}.
      * @return the {@link Node} of the specific origin
      */
     public Node getOrCreateOriginNode(String origin) {
@@ -72,7 +74,7 @@ public class ClusterNode extends StatisticNode {
                 if (statisticNode == null) {
                     // The node is absent, create a new node for the origin.
                     statisticNode = new StatisticNode();
-                    HashMap<String, StatisticNode> newMap = new HashMap<>(originCountMap.size() + 1);
+                    Map<String, StatisticNode> newMap = buildLRUCache();
                     newMap.putAll(originCountMap);
                     newMap.put(origin, statisticNode);
                     originCountMap = newMap;
@@ -88,11 +90,18 @@ public class ClusterNode extends StatisticNode {
         return originCountMap;
     }
 
+    private LRUCache<String, StatisticNode> buildLRUCache() {
+        return new LRUCache<String, StatisticNode>(
+            Integer.getInteger("csp.sentinel.origin.count", LRUCache.DEFAULT_CACHE_SIZE));
+    }
+
     /**
      * Add exception count only when given {@code throwable} is not a {@link BlockException}.
      *
-     * @param throwable target exception
-     * @param count     count to add
+     * @param throwable
+     *            target exception
+     * @param count
+     *            count to add
      */
     public void trace(Throwable throwable, int count) {
         if (count <= 0) {
---
+++ sentinel-core/src/test/java/com/alibaba/csp/sentinel/util/LRUCacheTest.java
@@ -0,0 +1,51 @@
+package com.alibaba.csp.sentinel.util;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertArrayEquals;
+
+/***
+ * 
+ * @author yikangfeng
+ * @date 2019/07/18
+ */
+public class LRUCacheTest {
+
+    @Test
+    public void testLRUCacheSize() {
+        final LRUCache<String, String> lruCache = buildLRUCache();
+        lruCache.put("a", "a");
+        lruCache.put("b", "b");
+        lruCache.put("c", "c");
+        lruCache.put("d", "d");
+
+        assertEquals(lruCache.getSize(), lruCache.size());
+
+        assertNull(lruCache.get("a"));
+
+    }
+
+    @Test
+    public void testLRUCacheAddItem() {
+        final LRUCache<String, String> lruCache = buildLRUCache();
+        lruCache.put("a", "a");
+        lruCache.put("b", "b");
+        lruCache.put("c", "c");
+        lruCache.put("d", "d");
+
+        lruCache.get("b");
+
+        final LRUCache<String, String> newCache = buildLRUCache();
+        newCache.putAll(lruCache);
+        newCache.put("e", "e");
+
+        assertArrayEquals(new String[] {"d", "b", "e"}, newCache.keySet().toArray(new String[0]));
+
+    }
+
+    private LRUCache<String, String> buildLRUCache() {
+        return new LRUCache<String, String>(3);
+    }
+}
