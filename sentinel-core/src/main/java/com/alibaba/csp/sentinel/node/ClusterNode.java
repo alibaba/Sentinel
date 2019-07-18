88344d41dc60e1e6aa1cb90f3bfba33baad31a40
yikangfeng <kangfeng01@126.com>
2019-07-18 19:51:50 +0800

    format code

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/node/ClusterNode.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/node/ClusterNode.java
@@ -1,16 +1,19 @@
-package com.alibaba.csp.sentinel.node;
 /*
  * Copyright 1999-2018 Alibaba Group Holding Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+package com.alibaba.csp.sentinel.node;
 
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
@@ -21,14 +24,16 @@ import com.alibaba.csp.sentinel.util.LRUCache;
 
 /**
  * <p>
- * This class stores summary runtime statistics of the resource, including rt, thread count, qps and so on. Same
- * resource shares the same {@link ClusterNode} globally, no matter in which
+ * This class stores summary runtime statistics of the resource, including rt, thread count, qps
+ * and so on. Same resource shares the same {@link ClusterNode} globally, no matter in which
  * {@link com.alibaba.csp.sentinel.context.Context}.
  * </p>
  * <p>
- * To distinguish invocation from different origin (declared in {@link ContextUtil#enter(String name, String origin)}),
- * one {@link ClusterNode} holds an {@link #originCountMap}, this map holds {@link StatisticNode} of different origin.
- * Use {@link #getOrCreateOriginNode(String)} to get {@link Node} of the specific origin.<br/>
+ * To distinguish invocation from different origin (declared in
+ * {@link ContextUtil#enter(String name, String origin)}),
+ * one {@link ClusterNode} holds an {@link #originCountMap}, this map holds {@link StatisticNode}
+ * of different origin. Use {@link #getOrCreateOriginNode(String)} to get {@link Node} of the specific
+ * origin.<br/>
  * Note that 'origin' usually is Service Consumer's app name.
  * </p>
  *
