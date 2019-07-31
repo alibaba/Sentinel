2375ae46e2a3564cf286b426442b564e0a47f5af
Lin.Liang <546648227@qq.com>
2019-07-31 23:05:08 +0800

    dashboard: fix AuthorityRuleEntity and ParamFlowRuleEntity fastjson serialize problem (#889)
    * Add @JsonIgnore annotation

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/datasource/entity/rule/ParamFlowRuleEntity.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/datasource/entity/rule/ParamFlowRuleEntity.java
@@ -15,14 +15,14 @@
  */
 package com.alibaba.csp.sentinel.dashboard.datasource.entity.rule;
 
-import java.util.List;
-
 import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowClusterConfig;
 import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowItem;
 import com.alibaba.csp.sentinel.slots.block.flow.param.ParamFlowRule;
 import com.alibaba.csp.sentinel.util.AssertUtil;
-
+import com.alibaba.fastjson.annotation.JSONField;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.util.List;
 
 /**
  * @author Eric Zhao
@@ -30,7 +30,8 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
  */
 public class ParamFlowRuleEntity extends AbstractRuleEntity<ParamFlowRule> {
 
-    public ParamFlowRuleEntity() {}
+    public ParamFlowRuleEntity() {
+    }
 
     public ParamFlowRuleEntity(ParamFlowRule rule) {
         AssertUtil.notNull(rule, "Authority rule should not be null");
@@ -46,61 +47,73 @@ public class ParamFlowRuleEntity extends AbstractRuleEntity<ParamFlowRule> {
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public String getLimitApp() {
         return rule.getLimitApp();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public String getResource() {
         return rule.getResource();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public int getGrade() {
         return rule.getGrade();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public Integer getParamIdx() {
         return rule.getParamIdx();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public double getCount() {
         return rule.getCount();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public List<ParamFlowItem> getParamFlowItemList() {
         return rule.getParamFlowItemList();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public int getControlBehavior() {
         return rule.getControlBehavior();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public int getMaxQueueingTimeMs() {
         return rule.getMaxQueueingTimeMs();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public int getBurstCount() {
         return rule.getBurstCount();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public long getDurationInSec() {
         return rule.getDurationInSec();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public boolean isClusterMode() {
         return rule.isClusterMode();
     }
 
     @JsonIgnore
+    @JSONField(serialize = false)
     public ParamFlowClusterConfig getClusterConfig() {
         return rule.getClusterConfig();
     }
