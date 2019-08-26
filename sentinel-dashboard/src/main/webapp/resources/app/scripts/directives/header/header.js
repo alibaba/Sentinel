43486e7c1c8c4fda43f1e413f48ec0255a18fd02
于玉桔 <769213327@qq.com>
2019-08-26 10:54:27 +0800

    dashboard: Support configuration for disabling login (#997)

--- sentinel-dashboard/src/main/webapp/resources/app/scripts/directives/header/header.js
+++ sentinel-dashboard/src/main/webapp/resources/app/scripts/directives/header/header.js
@@ -11,8 +11,25 @@ angular.module('sentinelDashboardApp')
       restrict: 'E',
       replace: true,
       controller: function ($scope, $state, $window, AuthService) {
-        if (!$window.localStorage.getItem('session_sentinel_admin')) {
-          $state.go('login');
+        if (!$window.localStorage.getItem("session_sentinel_admin")) {
+          AuthService.check().success(function (data) {
+            if (data.code == 0) {
+              $window.localStorage.setItem('session_sentinel_admin', JSON.stringify(data.data));
+              handleLogout($scope, data.data.id)
+            } else {
+              $state.go('login');
+            }
+          });
+        } else {
+          handleLogout($scope, JSON.parse($window.localStorage.getItem("session_sentinel_admin")).id)
+        }
+
+        function handleLogout($scope, id) {
+          if (id == 'FAKE_EMP_ID') {
+            $scope.showLogout = false;
+          } else {
+            $scope.showLogout = true;
+          }
         }
 
         $scope.logout = function () {
