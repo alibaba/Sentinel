792072c74d01b40763036fd1ed8851f7f0f93828
Lin.Liang <546648227@qq.com>
2019-10-10 11:19:27 +0800

    Add Etcd data-source extension (#1018)

---
+++ sentinel-extension/sentinel-datasource-etcd/README.md
@@ -0,0 +1,40 @@
+# Sentinel DataSource Etcd
+
+Sentinel DataSource Etcd provides integration with Etcd so that Etcd
+can be the dynamic rule data source of Sentinel. The data source uses push model (watcher).
+
+To use Sentinel DataSource Etcd, you should add the following dependency:
+
+```xml
+<dependency>
+    <groupId>com.alibaba.csp</groupId>
+    <artifactId>sentinel-datasource-etcd</artifactId>
+    <version>x.y.z</version>
+</dependency>
+```
+Configure Etcd Connect Properties By Config File (for example sentinel.properties)
+
+```
+csp.sentinel.etcd.end.points=http://ip1:port1,http://ip2:port2
+csp.sentinel.etcd.user=your_user
+csp.sentinel.etcd.password=your_password
+csp.sentinel.etcd.charset=your_charset
+csp.sentinel.etcd.auth.enable=true //if ture open user/password or ssl check
+csp.sentinel.etcd.authority=authority //ssl
+```
+or JVM args(Add -D prefix)
+
+
+Then you can create an `EtcdDataSource` and register to rule managers.
+For instance:
+
+```java
+  //`rule_key` is the rule config key
+  ReadableDataSource<String, List<FlowRule>> flowRuleEtcdDataSource = new EtcdDataSource<>(rule_key, (rule) -> JSON.parseArray(rule, FlowRule.class));
+  FlowRuleManager.register2Property(flowRuleEtcdDataSource.getProperty());
+```
+
+> Note: It needs to update JDK version to JDK8
+
+
+We've also provided an example: [sentinel-demo-etcd-datasource](https://github.com/alibaba/Sentinel/tree/master/sentinel-demo/sentinel-demo-etcd-datasource)
---
+++ sentinel-extension/sentinel-datasource-etcd/src/main/java/com/alibaba/csp/sentinel/datasource/etcd/EtcdConfig.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.alibaba.csp.sentinel.datasource.etcd;
+
+import com.alibaba.csp.sentinel.config.SentinelConfig;
+import com.alibaba.csp.sentinel.util.StringUtil;
+
+/**
+ * Configure Etcd Connect Properties
+ *
+ * @author lianglin
+ * @since 1.7.0
+ */
+public final class EtcdConfig {
+
+
+    public final static String END_POINTS = "csp.sentinel.etcd.end.points";
+    public final static String USER = "csp.sentinel.etcd.user";
+    public final static String PASSWORD = "csp.sentinel.etcd.password";
+    public final static String CHARSET = "csp.sentinel.etcd.charset";
+    public final static String AUTH_ENABLE = "csp.sentinel.etcd.auth.enable";
+    public final static String AUTHORITY = "csp.sentinel.etcd.authority";
+
+    private final static String ENABLED = "true";
+
+
+    public static String getEndPoints() {
+        return SentinelConfig.getConfig(END_POINTS);
+    }
+
+    public static String getUser() {
+        return SentinelConfig.getConfig(USER);
+    }
+
+    public static String getPassword() {
+        return SentinelConfig.getConfig(PASSWORD);
+    }
+
+    public static String getCharset() {
+        String etcdCharSet = SentinelConfig.getConfig(CHARSET);
+        if (StringUtil.isNotBlank(etcdCharSet)) {
+            return etcdCharSet;
+        }
+        return SentinelConfig.charset();
+    }
+
+    public static boolean isAuthEnable() {
+        if (ENABLED.equalsIgnoreCase(SentinelConfig.getConfig(AUTH_ENABLE))) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String getAuthority() {
+        return SentinelConfig.getConfig(AUTHORITY);
+    }
+
+    private EtcdConfig() {
+    };
+
+}
