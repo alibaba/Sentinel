22e68201ecdfb94aeb4ba88af70fda3b0fa3e526
zhangkai <zhangkai@czb365.com>
2019-10-18 00:12:37 +0800

    add java doc

--- sentinel-adapter/sentinel-spring-webmvc-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/spring/webmvc/callback/UrlCleaner.java
+++ sentinel-adapter/sentinel-spring-webmvc-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/spring/webmvc/callback/UrlCleaner.java
@@ -1,9 +1,15 @@
 package com.alibaba.csp.sentinel.adapter.spring.webmvc.callback;
 
 /**
+ * Clean sentinel target
  * @author zhangkai
  */
 public interface UrlCleaner {
 
+    /**
+     * Clean sentinel target
+     * @param originUrl
+     * @return
+     */
     String clean(String originUrl);
 }
