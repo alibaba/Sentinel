56d8856f69aaf7039f3397aa973d5f0a04b2fe04
于玉桔 <zhaoyuguang@apache.org>
2019-11-06 10:42:50 +0800

    Support customized block page HTTP status in sentinel-web-servlet-adapter (#1112)
    * Return canonical status 429 in the default block handler of sentinel-web-servlet-adapter.
    * Add a `csp.sentinel.web.servlet.block.page.http.status` property to support customized block status configuration.

--- sentinel-adapter/sentinel-web-servlet/src/main/java/com/alibaba/csp/sentinel/adapter/servlet/config/WebServletConfig.java
+++ sentinel-adapter/sentinel-web-servlet/src/main/java/com/alibaba/csp/sentinel/adapter/servlet/config/WebServletConfig.java
@@ -18,8 +18,10 @@ package com.alibaba.csp.sentinel.adapter.servlet.config;
 import com.alibaba.csp.sentinel.adapter.servlet.CommonFilter;
 import com.alibaba.csp.sentinel.adapter.servlet.CommonTotalFilter;
 import com.alibaba.csp.sentinel.config.SentinelConfig;
+import com.alibaba.csp.sentinel.util.StringUtil;
 
 /**
+ * @author zhaoyuguang
  * @author leyou
  */
 public class WebServletConfig {
@@ -27,6 +29,10 @@ public class WebServletConfig {
     public static final String WEB_SERVLET_CONTEXT_NAME = "sentinel_web_servlet_context";
 
     public static final String BLOCK_PAGE = "csp.sentinel.web.servlet.block.page";
+
+    public static final String BLOCK_PAGE_HTTP_STATUS = "csp.sentinel.web.servlet.block.page.http.status";
+
+    private static final int HTTP_STATUS_TOO_MANY_REQUESTS = 429;
 
     /**
      * Get redirecting page when Sentinel blocking for {@link CommonFilter} or
@@ -40,5 +46,32 @@ public class WebServletConfig {
 
     public static void setBlockPage(String blockPage) {
         SentinelConfig.setConfig(BLOCK_PAGE, blockPage);
+    }
+
+    /**
+     * Return status 429 in the default block page,
+     * you can use -Dcsp.sentinel.web.servlet.block.page.http.status=200 or other http status,
+     * to set http status which you want of the default block page.
+     * When csp.sentinel.web.servlet.block.page.http.status is empty or not number,
+     * the block page http status will be automatically set to 429(Too Many Requests).
+     *
+     * @return block page http status
+     */
+    public static int getBlockPageHttpStatus() {
+        String value = SentinelConfig.getConfig(BLOCK_PAGE_HTTP_STATUS);
+        if (StringUtil.isEmpty(value)) {
+            setBlockPageHttpStatus(HTTP_STATUS_TOO_MANY_REQUESTS);
+            return HTTP_STATUS_TOO_MANY_REQUESTS;
+        }
+        try {
+            return Integer.parseInt(SentinelConfig.getConfig(BLOCK_PAGE_HTTP_STATUS));
+        } catch (NumberFormatException e) {
+            setBlockPageHttpStatus(HTTP_STATUS_TOO_MANY_REQUESTS);
+        }
+        return HTTP_STATUS_TOO_MANY_REQUESTS;
+    }
+
+    public static void setBlockPageHttpStatus(int httpStatus) {
+        SentinelConfig.setConfig(BLOCK_PAGE_HTTP_STATUS, String.valueOf(httpStatus));
     }
 }
