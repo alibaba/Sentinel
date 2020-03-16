d884d406dd29b45eb6ad02da928cd50720b6fc5c
Eric Zhao <sczyh16@gmail.com>
2020-03-16 10:00:46 +0800

    Polish logging SPI related code and add general JUL adapter for Logger SPI (#1338)
    * Move the legacy JUL methods from LogBase to BaseJulLogger.
    * Add a JavaLoggingAdapter as the general JUL adapter for the Logger SPI, which makes it convenient to use (as the default logger).
    * Add LoggerSpiProvider to resolve Logger SPI.
    * Polish the logback demo.
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/log/jul/ConsoleHandler.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/log/jul/ConsoleHandler.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.alibaba.csp.sentinel.log;
+package com.alibaba.csp.sentinel.log.jul;
 
 import java.io.UnsupportedEncodingException;
 import java.util.logging.*;
