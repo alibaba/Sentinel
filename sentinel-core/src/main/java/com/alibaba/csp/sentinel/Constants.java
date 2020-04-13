63b8d62a29098905347d5d100ad14469930f06ac
Eric Zhao <sczyh16@gmail.com>
2020-04-13 20:19:22 +0800

    Bump version to 1.8.0-SNAPSHOT
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/Constants.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/Constants.java
@@ -31,7 +31,7 @@ import com.alibaba.csp.sentinel.util.VersionUtil;
  */
 public final class Constants {
 
-    public static final String SENTINEL_VERSION = VersionUtil.getVersion("1.7.2");
+    public static final String SENTINEL_VERSION = VersionUtil.getVersion("1.8.0");
 
     public final static int MAX_CONTEXT_NAME_SIZE = 2000;
     public final static int MAX_SLOT_CHAIN_SIZE = 6000;
