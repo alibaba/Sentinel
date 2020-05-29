6b86721b36929b21e19497526604456edfb8b388
于玉桔 <zhaoyuguang@apache.org>
2020-05-29 13:49:00 +0800

    Support setting class-level defaultFallback for annotation extension (#1493)

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/annotation/SentinelResource.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/annotation/SentinelResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ * Copyright 1999-2020 Alibaba Group Holding Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,9 +23,10 @@ import java.lang.annotation.*;
  * The annotation indicates a definition of Sentinel resource.
  *
  * @author Eric Zhao
+ * @author zhaoyuguang
  * @since 0.1.1
  */
-@Target(ElementType.METHOD)
+@Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 public @interface SentinelResource {
