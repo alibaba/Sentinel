b126767d655b774cb1c54b3042881d062dd9eab9
Eric Zhao <sczyh16@gmail.com>
2020-08-03 15:21:03 +0800

    Remove deprecated passCheck() in Rule and polish interface
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/authority/AuthorityRule.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/authority/AuthorityRule.java
@@ -15,8 +15,6 @@
  */
 package com.alibaba.csp.sentinel.slots.block.authority;
 
-import com.alibaba.csp.sentinel.context.Context;
-import com.alibaba.csp.sentinel.node.DefaultNode;
 import com.alibaba.csp.sentinel.slots.block.AbstractRule;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 
@@ -57,11 +55,6 @@ public class AuthorityRule extends AbstractRule {
         int result = super.hashCode();
         result = 31 * result + strategy;
         return result;
-    }
-
-    @Override
-    public boolean passCheck(Context context, DefaultNode node, int count, Object... args) {
-        return true;
     }
 
     @Override
