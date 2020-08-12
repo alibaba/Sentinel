c84cfa62217c9e0a8912ed0baf8d7d29d9f2bc14
ZhiQiang Gu <43897640+yunfeiyanggzq@users.noreply.github.com>
2020-08-12 15:28:06 +0800

    Polish cluster flow control demo: add port in Nacos address (#1655)
    Signed-off-by: yunfeiyanggzq <yunfeiyang@buaa.edu.cn>

--- sentinel-demo/sentinel-demo-cluster/sentinel-demo-cluster-embedded/src/main/java/com/alibaba/csp/sentinel/demo/cluster/init/DemoClusterInitFunc.java
+++ sentinel-demo/sentinel-demo-cluster/sentinel-demo-cluster-embedded/src/main/java/com/alibaba/csp/sentinel/demo/cluster/init/DemoClusterInitFunc.java
@@ -49,7 +49,7 @@ public class DemoClusterInitFunc implements InitFunc {
 
     private static final String APP_NAME = AppNameUtil.getAppName();
 
-    private final String remoteAddress = "localhost";
+    private final String remoteAddress = "localhost:8848";
     private final String groupId = "SENTINEL_GROUP";
 
     private final String flowDataId = APP_NAME + DemoConstants.FLOW_POSTFIX;
