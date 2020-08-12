5b0ffa26b7bcb57941c1c6afed5964c8d22cc363
yunfei <yunfei@fordeal.com>
2020-08-12 21:04:06 +0800

    [enhancement]metrics extension need a tag to distinguish [IN] from [OUT]

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/metric/extension/callback/MetricEntryCallback.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/metric/extension/callback/MetricEntryCallback.java
@@ -1,8 +1,9 @@
 package com.alibaba.csp.sentinel.metric.extension.callback;
 
 import com.alibaba.csp.sentinel.context.Context;
-import com.alibaba.csp.sentinel.metric.extension.MetricExtensionProvider;
+import com.alibaba.csp.sentinel.metric.extension.AdvancedMetricExtension;
 import com.alibaba.csp.sentinel.metric.extension.MetricExtension;
+import com.alibaba.csp.sentinel.metric.extension.MetricExtensionProvider;
 import com.alibaba.csp.sentinel.node.DefaultNode;
 import com.alibaba.csp.sentinel.slotchain.ProcessorSlotEntryCallback;
 import com.alibaba.csp.sentinel.slotchain.ResourceWrapper;
@@ -15,20 +16,32 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
  * @since 1.6.1
  */
 public class MetricEntryCallback implements ProcessorSlotEntryCallback<DefaultNode> {
-    @Override
-    public void onPass(Context context, ResourceWrapper resourceWrapper, DefaultNode param,
-                       int count, Object... args) throws Exception {
-        for (MetricExtension m : MetricExtensionProvider.getMetricExtensions()) {
-            m.increaseThreadNum(resourceWrapper.getName(), args);
-            m.addPass(resourceWrapper.getName(), count, args);
-        }
-    }
+	@Override
+	public void onPass(Context context, ResourceWrapper resourceWrapper, DefaultNode param, int count, Object... args)
+			throws Exception {
+		for (MetricExtension m : MetricExtensionProvider.getMetricExtensions()) {
+			if (m instanceof AdvancedMetricExtension) {
+				((AdvancedMetricExtension) m).increaseThreadNum(resourceWrapper.getName(),
+						resourceWrapper.getEntryType().name(), args);
+				((AdvancedMetricExtension) m).addPass(resourceWrapper.getName(), resourceWrapper.getEntryType().name(),
+						count, args);
+			} else {
+				m.increaseThreadNum(resourceWrapper.getName(), args);
+				m.addPass(resourceWrapper.getName(), count, args);
+			}
+		}
+	}
 
-    @Override
-    public void onBlocked(BlockException ex, Context context, ResourceWrapper resourceWrapper,
-                          DefaultNode param, int count, Object... args) {
-        for (MetricExtension m : MetricExtensionProvider.getMetricExtensions()) {
-            m.addBlock(resourceWrapper.getName(), count, context.getOrigin(), ex, args);
-        }
-    }
+	@Override
+	public void onBlocked(BlockException ex, Context context, ResourceWrapper resourceWrapper, DefaultNode param,
+			int count, Object... args) {
+		for (MetricExtension m : MetricExtensionProvider.getMetricExtensions()) {
+			if (m instanceof AdvancedMetricExtension) {
+				((AdvancedMetricExtension) m).addBlock(resourceWrapper.getName(), resourceWrapper.getEntryType().name(),
+						count, context.getOrigin(), ex, args);
+			} else {
+				m.addBlock(resourceWrapper.getName(), count, context.getOrigin(), ex, args);
+			}
+		}
+	}
 }
