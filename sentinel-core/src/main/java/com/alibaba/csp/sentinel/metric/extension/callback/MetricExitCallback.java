fffc848d124ca4f0fa3f88ca8ca525dcb080f992
Bill Yip <yezaifei@163.com>
2020-08-18 16:55:30 +0800

    Add extended interface for metric extension hook to support distinguishing traffic type (#1665)
    - Add EntryType args to all hook methods

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/metric/extension/callback/MetricExitCallback.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/metric/extension/callback/MetricExitCallback.java
@@ -1,8 +1,9 @@
 package com.alibaba.csp.sentinel.metric.extension.callback;
 
 import com.alibaba.csp.sentinel.context.Context;
-import com.alibaba.csp.sentinel.metric.extension.MetricExtensionProvider;
+import com.alibaba.csp.sentinel.metric.extension.AdvancedMetricExtension;
 import com.alibaba.csp.sentinel.metric.extension.MetricExtension;
+import com.alibaba.csp.sentinel.metric.extension.MetricExtensionProvider;
 import com.alibaba.csp.sentinel.slotchain.ProcessorSlotExitCallback;
 import com.alibaba.csp.sentinel.slotchain.ResourceWrapper;
 import com.alibaba.csp.sentinel.util.TimeUtil;
@@ -15,22 +16,31 @@ import com.alibaba.csp.sentinel.util.TimeUtil;
  */
 public class MetricExitCallback implements ProcessorSlotExitCallback {
 
-    @Override
-    public void onExit(Context context, ResourceWrapper resourceWrapper, int count, Object... args) {
-        for (MetricExtension m : MetricExtensionProvider.getMetricExtensions()) {
-            if (context.getCurEntry().getBlockError() != null) {
-                continue;
-            }
-            String resource = resourceWrapper.getName();
-            long realRt = TimeUtil.currentTimeMillis() - context.getCurEntry().getCreateTimestamp();
-            m.addRt(resource, realRt, args);
-            m.addSuccess(resource, count, args);
-            m.decreaseThreadNum(resource, args);
-
-            Throwable ex = context.getCurEntry().getError();
-            if (ex != null) {
-                m.addException(resource, count, ex);
-            }
-        }
-    }
+	@Override
+	public void onExit(Context context, ResourceWrapper resourceWrapper, int count, Object... args) {
+		for (MetricExtension m : MetricExtensionProvider.getMetricExtensions()) {
+			if (context.getCurEntry().getBlockError() != null) {
+				continue;
+			}
+			String resource = resourceWrapper.getName();
+			String entryType = resourceWrapper.getEntryType().name();
+			Throwable ex = context.getCurEntry().getError();
+			long realRt = TimeUtil.currentTimeMillis() - context.getCurEntry().getCreateTimestamp();
+			if (m instanceof AdvancedMetricExtension) {
+				((AdvancedMetricExtension) m).addRt(resource, entryType, realRt, args);
+				((AdvancedMetricExtension) m).addSuccess(resource, entryType, count, args);
+				((AdvancedMetricExtension) m).decreaseThreadNum(resource, entryType, args);
+				if (null != ex) {
+					((AdvancedMetricExtension) m).addException(resource, entryType, count, ex);
+				}
+			} else {
+				m.addRt(resource, realRt, args);
+				m.addSuccess(resource, count, args);
+				m.decreaseThreadNum(resource, args);
+				if (null != ex) {
+					m.addException(resource, count, ex);
+				}
+			}
+		}
+	}
 }
