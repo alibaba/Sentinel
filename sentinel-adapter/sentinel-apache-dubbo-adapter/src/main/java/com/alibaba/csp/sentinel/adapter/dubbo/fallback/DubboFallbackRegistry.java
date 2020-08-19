377ec7ed11fd2de4c3b8928d76fb593aac4c0ef3
Eric Zhao <sczyh16@gmail.com>
2020-08-19 21:59:17 +0800

    Fix NPE bug and improve default fallback in Dubbo 2.7.x adapter
    - Fix NPE bug in consumer filter (when non-biz error occurred)
    - Improve default fallback in Dubbo 2.7.x adapter: convert the BlockException to a simple RuntimeException (with necessary message)
    - Polish code and comments
    
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-adapter/sentinel-apache-dubbo-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/dubbo/fallback/DubboFallbackRegistry.java
+++ sentinel-adapter/sentinel-apache-dubbo-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/dubbo/fallback/DubboFallbackRegistry.java
@@ -16,18 +16,14 @@
 package com.alibaba.csp.sentinel.adapter.dubbo.fallback;
 
 import com.alibaba.csp.sentinel.adapter.dubbo.config.DubboAdapterGlobalConfig;
-import com.alibaba.csp.sentinel.util.AssertUtil;
 
 /**
  * <p>Global fallback registry for Dubbo.</p>
  *
- * <p>
- * Note: Circuit breaking is mainly designed for consumer. The provider should not
- * give fallback result in most circumstances.
- * </p>
- *
  * @author Eric Zhao
+ * @deprecated use {@link DubboAdapterGlobalConfig} instead since 1.8.0.
  */
+@Deprecated
 public final class DubboFallbackRegistry {
 
     public static DubboFallback getConsumerFallback() {
@@ -35,7 +31,6 @@ public final class DubboFallbackRegistry {
     }
 
     public static void setConsumerFallback(DubboFallback consumerFallback) {
-        AssertUtil.notNull(consumerFallback, "consumerFallback cannot be null");
         DubboAdapterGlobalConfig.setConsumerFallback(consumerFallback);
     }
 
@@ -44,7 +39,6 @@ public final class DubboFallbackRegistry {
     }
 
     public static void setProviderFallback(DubboFallback providerFallback) {
-        AssertUtil.notNull(providerFallback, "providerFallback cannot be null");
         DubboAdapterGlobalConfig.setProviderFallback(providerFallback);
     }
 
