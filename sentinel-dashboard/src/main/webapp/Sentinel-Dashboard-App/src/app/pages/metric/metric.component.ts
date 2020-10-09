40e7b8e309f8855c7ed602625a91edd4aad529b7
wanghao <690324981@qq.com>
2020-10-09 17:41:03 +0800

    实时监控折线图完成 【暂存问题】第一次查询的时候获取不到echarts的实例，需要等第二次查询才能渲染。

--- sentinel-dashboard/src/main/webapp/Sentinel-Dashboard-App/src/app/pages/metric/metric.component.ts
+++ sentinel-dashboard/src/main/webapp/Sentinel-Dashboard-App/src/app/pages/metric/metric.component.ts
@@ -1,106 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { KieMetricService } from 'src/app/services/kie-metric/kie-metric.service';
 import { KieInfoService } from 'src/app/services/kie-info/kie-info.service';
-
 
 @Component({
   selector: 'app-metric',
   templateUrl: './metric.component.html',
   styleUrls: ['./metric.component.css']
 })
-export class MetricComponent implements OnInit {
-  chartOption: any = {};
+export class MetricComponent implements OnInit, OnDestroy {
+  chartOptionMap: any = {};
+  chartOptionTemp: any = {
+    title: {
+        text: ''
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['通过QPS', '拒绝QPS']
+    },
+    xAxis: {
+        type: 'category',
+        data: [],
+        axisLabel: {
+            interval: 30
+        }
+    },
+    yAxis: {
+        splitLine: {
+            show: false
+        }
+    },
+    series: [
+        {
+            color: 'green',
+            symbol: 'none',
+            name: '通过QPS',
+            type: 'line',
+            data: []
+        }, 
+        {
+            color: 'blue',
+            symbol: 'none',
+            name: '拒绝QPS',
+            type: 'line',
+            data: []
+        }
+    ]
+  };
   app: string;
   service: string;
   service_id: string;
-  listData: any;
-
+  listData: any[] = [];
+  intervalId: any;
   // [{ip: ip,port: port},...]
   instanceList: any = [];
   selectedIns: any;
+  INTERVAL_TIME: number = 5000;
 
   constructor(
     private route: ActivatedRoute,
     private kieMetricService: KieMetricService,
-    private kieInfoService: KieInfoService
+    private kieInfoService: KieInfoService,
+    private elementRef: ElementRef
   ) { }
 
-  async ngOnInit() {
+  async ngOnInit() {  
+    this.listData = [];
     this.route.params.subscribe(res => {
     this.app = res.app;
     this.service = res.service;
     this.service_id = res.service_id;
     });
-    this.listData = [
-        {
-          title: 'Ant Design Title 1'
-        },
-        {
-          title: 'Ant Design Title 2'
-        },
-        {
-          title: 'Ant Design Title 3'
-        },
-        {
-          title: 'Ant Design Title 5'
-        }
-    ];
-
-    this.chartOption = {
-        title: {
-            text: 'resource'
-        },
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data: ['通过QPS', '拒绝QPS']
-        },
-        grid: {
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: {
-            type: 'category',
-            boundaryGap: false,
-            data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-            splitLine: {
-                show: false
-            },
-        },
-        yAxis: {
-            type: 'value',
-            splitLine: {
-                show: false
-            },
-        },
-        series: [
-            {
-                color: 'green',
-                symbol: 'none',
-                name: '通过QPS',
-                type: 'line',
-                stack: '总量',
-                data: [120, 132, 101, 134, 90, 230, 210]
-                // data: [200, 200, 200, 200, 200, 200, 200]
-            },
-            {
-                color: 'blue',
-                symbol: 'none',
-                name: '拒绝QPS',
-                type: 'line',
-                stack: '总量',
-                data: [220, 182, 191, 234, 290, 330, 310]
-            }
-        ]
-    };  
-
-    console.log('----------');
-    console.log(this.chartOption.series[0].data);
 
     const instanceInfos: any = await new Promise(resolve => {
         this.kieInfoService.queryInstanceInfos(this.service_id).subscribe(res => {
@@ -116,57 +89,77 @@ export class MetricComponent implements OnInit {
         });
     });
     this.selectedIns = this.instanceList[0];
-    this.instanceList.push({
-        ip: "1234",
-        port: 123
-    });
-
-    // const metricInfos: any = await new Promise(resolve => {
-    //     var param = {
-    //         serviceId: this.service_id,
-    //         ip: this.selectedIns.ip,
-    //         port: this.selectedIns.port
-    //     };
-    //     this.kieMetricService.queryResourceMetric(param).subscribe(res => {
-    //         if (res.success) {
-    //             resolve(res);
-    //         }
-    //     });
-    // });
 
     var param = {
         serviceId: this.service_id,
         ip: this.selectedIns.ip,
         port: this.selectedIns.port
     };
-    setInterval(() => {
+    this.intervalId = setInterval(() => {
         this.kieMetricService.queryResourceMetric(param).subscribe(res => {
             if (res.success) {
-                console.log(res);
+                for (var key in res.data.metric) {
+                    if (!this.listData.includes(key)) {
+                        this.listData.push(key);
+                        this.chartOptionMap[key] = this.chartOptionTemp;
+                        this.chartOptionMap[key].title.text = key;
+                    }
+                    this.chartOptionMap[key].xAxis.data = [];
+                    this.chartOptionMap[key].series[0].data = [];
+                    this.chartOptionMap[key].series[1].data = [];
+                    res.data.metric[key].forEach(ele => {
+                        this.chartOptionMap[key].xAxis.data.push(new Date(ele.timestamp).toString().slice(16,24));
+                        this.chartOptionMap[key].series[0].data.push(ele.passQps);
+                        this.chartOptionMap[key].series[1].data.push(ele.blockQps);
+                    });
+                }
+                let mycharts = echarts.init(<HTMLDivElement>document.getElementById(key));
+                mycharts.setOption(this.chartOptionMap[key]);
             }
         });
-    }, 10000);
-
-    // console.log(metricInfos);
-    // metricInfos.data;
+    }, this.INTERVAL_TIME);
   }
 
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+
   public instanceChange(e): void {
-      console.log('instanceChange');
-      this.chartOption.series[0].data = [200, 200, 200, 200, 200, 200, 200];
-      console.log(this.chartOption);
-      console.log(this.chartOption.series[0]);
-      this.selectedIns = e;
-      var param = {
-          serviceId: this.service_id,
-          ip: this.selectedIns.ip,
-          port: this.selectedIns.port
-      };
-      this.kieMetricService.queryResourceMetric(param).subscribe(res => {
-          if (res.success) {
-            console.log("res", res);
-          }
-      });
+    // 清缓存数据
+    clearInterval(this.intervalId);
+    this.chartOptionMap = {};
+    this.listData = [];
+
+    this.selectedIns = e;
+    var param = {
+        serviceId: this.service_id,
+        ip: this.selectedIns.ip,
+        port: this.selectedIns.port
+    };
+    this.intervalId = setInterval(() => {
+    this.kieMetricService.queryResourceMetric(param).subscribe(res => {
+        if (res.success) {
+            for (var key in res.data.metric) {
+                if (!this.listData.includes(key)) {
+                    this.listData.push(key);
+                    this.chartOptionMap[key] = this.chartOptionTemp;
+                    this.chartOptionMap[key].title.text = key;
+                }
+                this.chartOptionMap[key].xAxis.data = [];
+                this.chartOptionMap[key].series[0].data = [];
+                this.chartOptionMap[key].series[1].data = [];
+                res.data.metric[key].forEach(ele => {
+                    this.chartOptionMap[key].xAxis.data.push(new Date(ele.timestamp).toString().slice(16,24));
+                    this.chartOptionMap[key].series[0].data.push(ele.passQps);
+                    this.chartOptionMap[key].series[1].data.push(ele.blockQps);
+                });
+            }
+            let mycharts = echarts.init(<HTMLDivElement>document.getElementById(key));
+            mycharts.setOption(this.chartOptionMap[key]);
+        }
+    });
+    }, this.INTERVAL_TIME);
   }
 
 }
---
+++ sentinel-dashboard/src/main/webapp/Sentinel-Dashboard-App/src/typing.d.ts
@@ -0,0 +1,1 @@
+declare var echart: any;
