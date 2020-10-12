a89af9734a6caeb5b2403e4fc0ba6ebffa868cc9
解建君 <5661954@qq.com>
2020-10-12 18:52:41 +0800

    dashboard 使用influxdb

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/config/MetricsConfig.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/config/MetricsConfig.java
@@ -1,35 +1,51 @@
 package com.alibaba.csp.sentinel.dashboard.config;
 
 import com.alibaba.csp.sentinel.dashboard.client.InfluxDbUtils;
+import com.alibaba.csp.sentinel.dashboard.repository.metric.InMemoryMetricsRepository;
 import com.alibaba.csp.sentinel.dashboard.repository.metric.InfluxDBMetricsRepository;
 import com.alibaba.csp.sentinel.dashboard.repository.metric.MetricsRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "spring.influx")
 public class MetricsConfig {
-    public String database;
-    private String retentionPolicy;
 
-    @Bean
-    public InfluxDbUtils influxDbUtils(){
-        return new InfluxDbUtils(database);
+    @Configuration(proxyBeanMethods = false)
+    @ConfigurationProperties(prefix = "spring.influx")
+    protected static class MetricsConfiguration {
+        public String database;
+        private String retentionPolicy;
+
+        @Bean
+        @ConditionalOnProperty(value = "spring.influx.enabled",matchIfMissing = true)
+        public InfluxDbUtils influxDbUtils(){
+            return new InfluxDbUtils(database);
+        }
+
+        @Bean
+        @ConditionalOnProperty(value = "spring.influx.enabled",matchIfMissing = true)
+        public MetricsRepository metricStore(){
+            return new InfluxDBMetricsRepository();
+        }
+
+        public void setDatabase(String database) {
+            this.database = database;
+        }
+
+        public void setRetentionPolicy(String retentionPolicy) {
+            this.retentionPolicy = retentionPolicy;
+        }
     }
 
-    @Bean
-    @ConditionalOnMissingBean(name = "influxDb")
-    public MetricsRepository metricStore(){
-        return new InfluxDBMetricsRepository();
-    }
-
-    public void setDatabase(String database) {
-        this.database = database;
-    }
-
-    public void setRetentionPolicy(String retentionPolicy) {
-        this.retentionPolicy = retentionPolicy;
+    @Configuration(proxyBeanMethods = false)
+    protected static class EmbeddedMetricsConfiguration {
+        @Bean
+        @ConditionalOnMissingBean(name={"metricStore"})
+        public MetricsRepository metricStore(){
+            return new InMemoryMetricsRepository();
+        }
     }
 }
