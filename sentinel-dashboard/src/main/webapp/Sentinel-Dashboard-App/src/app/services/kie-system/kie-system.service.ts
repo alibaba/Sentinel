455dd064cd1f1c11db12f11dce56a70b472cb5b4
IDonotK <LuKick@126.com>
2020-10-19 19:40:15 +0800

    增加接口参数null过滤

--- sentinel-dashboard/src/main/webapp/Sentinel-Dashboard-App/src/app/services/kie-system/kie-system.service.ts
+++ sentinel-dashboard/src/main/webapp/Sentinel-Dashboard-App/src/app/services/kie-system/kie-system.service.ts
@@ -11,19 +11,29 @@ export class KieSystemService {
     private http: HttpClient
   ) { }
 
+  public filterParams(params): any {
+      const filterRes = {};
+      for (let key in params) {
+          if (params[key] !== null) {
+            filterRes[key] = params[key];
+          }
+      }
+      return filterRes;
+  }
+
   public queryKieSystemRules(service_id: string): Observable<any> {
     return this.http.get(`/kie/system/rules?serverId=${service_id}`);
   }
 
-  public createKieSystemRule(service_id: string, param: any): Observable<any> {
-    return this.http.post(`/kie/system/${service_id}/rule`, param);
+  public createKieSystemRule(service_id: string, params: any): Observable<any> {
+    return this.http.post(`/kie/system/${service_id}/rule`, this.filterParams(params));
   }
 
   public deleteKieSystemRule(service_id: string, rule_id: string): Observable<any> {
     return this.http.delete(`/kie/system/${service_id}/rule/${rule_id}`);
   }
 
-  public updateKieSystemRule(service_id: string, param: any): Observable<any> {
-    return this.http.put(`/kie/system/${service_id}/rule`, param);
+  public updateKieSystemRule(service_id: string, params: any): Observable<any> {
+    return this.http.put(`/kie/system/${service_id}/rule`, this.filterParams(params));
   }
 }
