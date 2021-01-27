7fe09ff0724d6f401e50151c26ce01d566912402
cdfive <31885791+cdfive@users.noreply.github.com>
2021-01-27 09:45:56 +0800

    Refactor SpiLoader and enhance SPI mechanism (#1383)
    * Add `@Spi` annotation as the general annotation for SPI definition.
    * Add isDefault in @Spi, add loadDefaultInstance and improve loadFirstInstanceOrDefault method, improve test cases
    * Add SpiLoaderException class for thrown when something goes wrong while loading Provider
    * Rearrange packages of base SPI mechanism
    
    NOTE: this PR contains breaking changes regarding API.

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/authority/AuthoritySlot.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/block/authority/AuthoritySlot.java
@@ -18,12 +18,13 @@ package com.alibaba.csp.sentinel.slots.block.authority;
 import java.util.Map;
 import java.util.Set;
 
+import com.alibaba.csp.sentinel.Constants;
 import com.alibaba.csp.sentinel.context.Context;
 import com.alibaba.csp.sentinel.node.DefaultNode;
 import com.alibaba.csp.sentinel.slotchain.AbstractLinkedProcessorSlot;
 import com.alibaba.csp.sentinel.slotchain.ProcessorSlot;
 import com.alibaba.csp.sentinel.slotchain.ResourceWrapper;
-import com.alibaba.csp.sentinel.spi.SpiOrder;
+import com.alibaba.csp.sentinel.spi.Spi;
 
 /**
  * A {@link ProcessorSlot} that dedicates to {@link AuthorityRule} checking.
@@ -31,7 +32,7 @@ import com.alibaba.csp.sentinel.spi.SpiOrder;
  * @author leyou
  * @author Eric Zhao
  */
-@SpiOrder(-6000)
+@Spi(order = Constants.ORDER_AUTHORITY_SLOT)
 public class AuthoritySlot extends AbstractLinkedProcessorSlot<DefaultNode> {
 
     @Override
