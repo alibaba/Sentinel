149cf8830b510e0ffccce220a525b8cf5f72fa08
Eric Zhao <sczyh16@gmail.com>
2021-02-01 13:57:15 +0800

    Use JDK 1.8 native LongAdder instead and remove legacy LongAdder
    Signed-off-by: Eric Zhao <sczyh16@gmail.com>

--- sentinel-cluster/sentinel-cluster-server-default/src/main/java/com/alibaba/csp/sentinel/cluster/flow/statistic/metric/ClusterParamMetric.java
+++ sentinel-cluster/sentinel-cluster-server-default/src/main/java/com/alibaba/csp/sentinel/cluster/flow/statistic/metric/ClusterParamMetric.java
@@ -15,12 +15,18 @@
  */
 package com.alibaba.csp.sentinel.cluster.flow.statistic.metric;
 
-import com.alibaba.csp.sentinel.slots.statistic.base.LongAdder;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.concurrent.atomic.LongAdder;
+
 import com.alibaba.csp.sentinel.slots.statistic.cache.CacheMap;
 import com.alibaba.csp.sentinel.util.AssertUtil;
-
-import java.util.*;
-import java.util.Map.Entry;
 
 /**
  * @author Eric Zhao
