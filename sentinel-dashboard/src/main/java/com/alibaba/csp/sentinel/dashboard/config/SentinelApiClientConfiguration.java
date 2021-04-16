70aa2b4c1e18a895a2f40c1412baa05d97ae7ed4
anilople <Anilople@outlook.com>
2021-04-16 18:40:04 +0800

    docs: Add LICENSE to .java file

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/config/SentinelApiClientConfiguration.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/config/SentinelApiClientConfiguration.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.csp.sentinel.dashboard.config;
 
 import com.alibaba.csp.sentinel.dashboard.client.DefaultSentinelApiClient;
@@ -7,9 +22,15 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author wxq
+ */
 @Configuration
 public class SentinelApiClientConfiguration {
 
+    /**
+     * @return default {@link SentinelApiClient} if user didn't define it
+     */
     @Bean
     @ConditionalOnMissingBean
     public SentinelApiClient sentinelApiClient(AppManagement appManagement) {
