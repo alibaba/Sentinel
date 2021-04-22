8ddd1d14be1a5cb2d1caf31dd4d39adf7beaae33
anilople <15523186+Anilople@users.noreply.github.com>
2021-04-22 20:46:39 +0800

    test(sentinel-transport-common): forbid change message

--- sentinel-transport/sentinel-transport-common/src/test/java/com/alibaba/csp/sentinel/transport/message/AbstractHeartbeatMessageTest.java
+++ sentinel-transport/sentinel-transport-common/src/test/java/com/alibaba/csp/sentinel/transport/message/AbstractHeartbeatMessageTest.java
@@ -15,7 +15,8 @@
  */
 package com.alibaba.csp.sentinel.transport.message;
 
-import static org.junit.Assert.*;
+import static com.alibaba.csp.sentinel.transport.message.HeartbeatMessageKeyConstants.HOST_NAME;
+import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
@@ -33,6 +34,20 @@ public class AbstractHeartbeatMessageTest {
 		HeartbeatMessage heartbeatMessage = new BeforeGetHeartbeatMessageTest();
 		assertEquals("1", heartbeatMessage.get().get("count"));
 		assertEquals("2", heartbeatMessage.get().get("count"));
+	}
+	
+	@Test(expected = UnsupportedOperationException.class)
+	public void testModify() {
+		HeartbeatMessage heartbeatMessage = new EmptyHeartbeatMessageTest();
+		heartbeatMessage.get().put(HOST_NAME, "error host name");
+	}
+	
+	/**
+	 * @author wxq
+	 * @since 1.8.2
+	 */
+	private static class EmptyHeartbeatMessageTest extends AbstractHeartbeatMessage {
+		
 	}
 	
 	/**
