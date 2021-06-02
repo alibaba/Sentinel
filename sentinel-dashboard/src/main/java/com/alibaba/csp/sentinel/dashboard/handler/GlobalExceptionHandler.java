774aa0c2a8788a9adc195923c568d0b90b41e6f1
chenglu <1285823170@qq.com>
2021-06-02 19:07:48 +0800

    add license info

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/handler/GlobalExceptionHandler.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/handler/GlobalExceptionHandler.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 1999-2021 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.alibaba.csp.sentinel.dashboard.handler;
 
 import com.alibaba.csp.sentinel.dashboard.client.CommandNotFoundException;
-import com.alibaba.csp.sentinel.dashboard.controller.FlowControllerV1;
 import com.alibaba.csp.sentinel.dashboard.domain.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
