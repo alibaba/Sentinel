46c951605b1f14f0980ee9bcda420a0d52210cd9
sarihuang <280456134@qq.com>
2021-11-02 22:19:36 +0800

    delete useless imports and enum type

--- sentinel-transport/sentinel-transport-spring-mvc/src/main/java/com/alibaba/csp/sentinel/transport/command/http/StatusCode.java
+++ sentinel-transport/sentinel-transport-spring-mvc/src/main/java/com/alibaba/csp/sentinel/transport/command/http/StatusCode.java
@@ -24,9 +24,6 @@ public enum StatusCode {
      */
     OK(200, "OK"),
     BAD_REQUEST(400, "Bad Request"),
-    REQUEST_TIMEOUT(408, "Request Timeout"),
-    LENGTH_REQUIRED(411, "Length Required"),
-    UNSUPPORTED_MEDIA_TYPE(415, "Unsupported Media Type"),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
     
     private int code;
