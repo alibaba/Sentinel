d43cfd0a3ea319727da4f91de1dc5288e4b37f62
jiajiangnan <jiajiangnan.hz@foxmail.com>
2022-01-19 10:44:19 +0800

    please add the license for classses.

--- sentinel-extension/sentinel-datasource-nacos/src/main/java/com/alibaba/csp/sentinel/datasource/nacos/NacosDataSource.java
+++ sentinel-extension/sentinel-datasource-nacos/src/main/java/com/alibaba/csp/sentinel/datasource/nacos/NacosDataSource.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2020 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.csp.sentinel.datasource.nacos;
 
 import com.alibaba.csp.sentinel.datasource.*;
@@ -27,7 +42,7 @@ public class NacosDataSource<T> extends SentinelDataSource {
 
     /**
      * @deprecated since 1.8.4.
-     * Only for compatible with older versions < 1.8.4-SNAPSHOT
+     * Only for compatible with older versions < 1.8.4
      * Constructs an read-only DataSource with Nacos backend.
      *
      * @param serverAddr server address of Nacos, cannot be empty
@@ -47,7 +62,7 @@ public class NacosDataSource<T> extends SentinelDataSource {
 
     /**
      * @deprecated since 1.8.4.
-     * Only for compatible with older versions < 1.8.4-SNAPSHOT
+     * Only for compatible with older versions < 1.8.4
      *
      * @param properties properties for construct {@link ConfigService} using {@link NacosFactory#createConfigService(Properties)}
      * @param groupId    group ID, cannot be empty
