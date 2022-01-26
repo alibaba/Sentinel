cd8b2f61391c8eaa61e41826c32a69b455e6c4bc
roy500 <1010690219@qq.com>
2022-01-26 09:39:58 +0800

    change the code indent

--- sentinel-adapter/sentinel-spring-webmvc-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/spring/webmvc/license/SentinelAfterCompletionUtil.java
+++ sentinel-adapter/sentinel-spring-webmvc-adapter/src/main/java/com/alibaba/csp/sentinel/adapter/spring/webmvc/license/SentinelAfterCompletionUtil.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 /**
  * Exception interception statistics
- *
+ * 
  * @author Roy
  * @date 2022/1/249:15
  */
@@ -17,7 +17,7 @@ public class SentinelAfterCompletionUtil {
     /**
      * The afterCompletion method of the HandlerInterceptor does not catch exception statistics when using @ControllerAdvice for global exception fetching
      * Call SentinelAfterException.exit(request, ex) method to realize exception statistics
-     *
+     * 
      * @param request
      * @param ex
      */
