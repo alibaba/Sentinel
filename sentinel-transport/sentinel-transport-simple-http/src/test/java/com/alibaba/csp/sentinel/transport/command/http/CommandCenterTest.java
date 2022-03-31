7823d2759fe88a189e39221ff6a2056e90fd19f1
dane brown <tain198127@163.com>
2022-04-01 01:04:35 +0800

    When CommandCenter not start completely, invoke stop method will throw a null point exception. It's a fatal error.

--- sentinel-transport/sentinel-transport-simple-http/src/test/java/com/alibaba/csp/sentinel/transport/command/http/CommandCenterTest.java
+++ sentinel-transport/sentinel-transport-simple-http/src/test/java/com/alibaba/csp/sentinel/transport/command/http/CommandCenterTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alibaba.csp.sentinel.transport.command.http;
 
 import com.alibaba.csp.sentinel.command.CommandCenterProvider;
@@ -5,12 +20,6 @@ import com.alibaba.csp.sentinel.transport.CommandCenter;
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Created by danebrown on 2022/4/1
- * mail: tain198127@163.com
- *
- * @author danebrown
- */
 public class CommandCenterTest {
     @Test
     public void stopCommandCenter(){
