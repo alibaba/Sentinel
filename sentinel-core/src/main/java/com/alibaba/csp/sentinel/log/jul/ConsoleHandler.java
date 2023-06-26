ae42ddd35fe1e7ee4a712c0cf97846b362e78564
karl-sy <71377602+karl-sy@users.noreply.github.com>
2023-06-26 13:42:50 +0800

    Make default JUL-based logging asynchronous (#3136)
    * fix issues/1463 : Make default JUL-based logging asynchronous

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/log/jul/ConsoleHandler.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/log/jul/ConsoleHandler.java
@@ -15,7 +15,16 @@
  */
 package com.alibaba.csp.sentinel.log.jul;
 
+import com.alibaba.csp.sentinel.concurrent.NamedThreadFactory;
+
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.*;
 
 /**
@@ -43,9 +52,21 @@ class ConsoleHandler extends Handler {
      */
     private StreamHandler stderrHandler;
 
+    private ExecutorService executor;
+
     public ConsoleHandler() {
         this.stdoutHandler = new StreamHandler(System.out, new CspFormatter());
         this.stderrHandler = new StreamHandler(System.err, new CspFormatter());
+
+        int corePoolSize = 1;
+        int maximumPoolSize = 1;
+        long keepAliveTime = 0;
+        /**insure the log can be recorded*/
+        int queueSize = 1024;
+        RejectedExecutionHandler handler = new LogRejectedExecutionHandler();
+        executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize,
+                keepAliveTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(queueSize),
+                new NamedThreadFactory("sentinel-console-log-executor", true), handler);
     }
 
     @Override
@@ -62,13 +83,7 @@ class ConsoleHandler extends Handler {
 
     @Override
     public void publish(LogRecord record) {
-        if (record.getLevel().intValue() >= Level.WARNING.intValue()) {
-            stderrHandler.publish(record);
-            stderrHandler.flush();
-        } else {
-            stdoutHandler.publish(record);
-            stdoutHandler.flush();
-        }
+        executor.execute(new LogTask(record,stdoutHandler,stderrHandler));
     }
 
     @Override
@@ -79,7 +94,67 @@ class ConsoleHandler extends Handler {
 
     @Override
     public void close() throws SecurityException {
+        /**not need to record log if process is killed.*/
+        executor.shutdown();
         stdoutHandler.close();
         stderrHandler.close();
     }
+
+    public ExecutorService getExecutor() {
+        return executor;
+    }
+
+    static class LogRejectedExecutionHandler implements RejectedExecutionHandler {
+
+        /**
+         * The period of logged rejected records.
+         */
+        private final long recordPeriod;
+
+        private Long lastRecordTime;
+
+        public LogRejectedExecutionHandler() {
+            String DEFAULT_REJECTED_RECORD_PERIOD = "60000";
+            String REJECTED_RECORD_PERIOD_KEY = "sentinel.rejected.record.period";
+            lastRecordTime = null;
+            recordPeriod = Long.parseLong(System.getProperty(REJECTED_RECORD_PERIOD_KEY, DEFAULT_REJECTED_RECORD_PERIOD));
+        }
+
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            long currentTimestamp = System.currentTimeMillis();
+            if (lastRecordTime == null || currentTimestamp - lastRecordTime > recordPeriod) {
+                System.err.println("Failed to log sentinel record with console, rejected");
+                lastRecordTime = currentTimestamp;
+            }
+        }
+
+    }
+
+    static class LogTask implements Runnable {
+        private final LogRecord record;
+        private final StreamHandler stdoutHandler;
+        private final StreamHandler stderrHandler;
+
+        public LogTask(LogRecord record,StreamHandler stdoutHandler,StreamHandler stderrHandler) {
+            this.record = record;
+            this.stdoutHandler = stdoutHandler;
+            this.stderrHandler = stderrHandler;
+        }
+
+        public void run() {
+            if (record.getLevel().intValue() >= Level.WARNING.intValue()) {
+                stderrHandler.publish(record);
+                stderrHandler.flush();
+            } else {
+                stdoutHandler.publish(record);
+                stdoutHandler.flush();
+            }
+        }
+
+        public LogRecord getRecord() {
+            return record;
+        }
+
+    }
+
 }
