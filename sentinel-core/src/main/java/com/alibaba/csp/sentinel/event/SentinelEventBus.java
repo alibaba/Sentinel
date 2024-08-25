5def0da0bb8a56d898ebaf46f16cbfc3c559b3be
Daydreamer-ia <2296032269@qq.com>
2024-08-25 21:13:31 +0800

    feature: basic support for sentinel event, interface and outer ops tools

---
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/event/SentinelEventBus.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 1999-2024 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.csp.sentinel.event;
+
+import com.alibaba.csp.sentinel.event.factory.SentinelEventMulticasterFactory;
+import com.alibaba.csp.sentinel.event.model.SentinelEvent;
+import com.alibaba.csp.sentinel.event.multicaster.SentinelEventMulticaster;
+import com.alibaba.csp.sentinel.event.registry.SentinelEventListenerRegistry;
+import com.alibaba.csp.sentinel.log.RecordLog;
+import com.alibaba.csp.sentinel.spi.SpiLoader;
+
+import java.util.Properties;
+
+/**
+ * Ops of sentinel event.
+ *
+ * @author Daydreamer-ia
+ */
+public class SentinelEventBus {
+
+    /**
+     * INSTANCE.
+     */
+    private static final SentinelEventBus INSTANCE = new SentinelEventBus();
+
+    /**
+     * mapping of event and multicaster.
+     */
+    private final SentinelEventMulticasterFactory eventMulticasterFactory;
+
+    /**
+     * event registry.
+     */
+    private final SentinelEventListenerRegistry sentinelEventListenerRegistry;
+
+    /**
+     * whether to enabled event support.
+     */
+    private volatile boolean enabledEvent = false;
+
+
+    private SentinelEventBus() {
+        // init
+        SentinelEventMulticasterFactory factory = SpiLoader.of(SentinelEventMulticasterFactory.class).loadHighestPriorityInstance();
+        SentinelEventListenerRegistry registry = SpiLoader.of(SentinelEventListenerRegistry.class).loadHighestPriorityInstance();
+        this.eventMulticasterFactory = factory;
+        this.sentinelEventListenerRegistry = registry;
+        // only when both of registry and factory are not null.
+        enabledEvent = (factory != null && registry != null);
+        init();
+        RecordLog.info("EnabledEventFeature: {} | SentinelEventMulticasterFactory: {} | SentinelEventListenerRegistry: {}",
+                this.enabledEvent, this.eventMulticasterFactory, this.sentinelEventListenerRegistry);
+    }
+
+    /**
+     * Init ops for registry and factory
+     */
+    private void init() {
+        this.sentinelEventListenerRegistry.init(getRegistryProperties());
+        this.eventMulticasterFactory.init(getFactoryProperties(), this.sentinelEventListenerRegistry);
+    }
+
+    /**
+     * Get factory properties.
+     *
+     * @return properties
+     */
+    private Properties getFactoryProperties() {
+        // load properties from sys properties if necessary
+        return new Properties();
+    }
+
+    /**
+     * Get registry properties.
+     *
+     * @return properties
+     */
+    private Properties getRegistryProperties() {
+        // load properties from sys properties if necessary
+        return new Properties();
+    }
+
+    /**
+     * Get instance of bus.
+     *
+     * @return event bus.
+     */
+    public static SentinelEventBus getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Register multicaster of event.
+     *
+     * @param clazz       event
+     * @param multicaster multicaster
+     * @return whether success to add
+     */
+    public boolean registerMulticaster(Class<? extends SentinelEvent> clazz, SentinelEventMulticaster multicaster) {
+        return eventMulticasterFactory.addSentinelEventMulticaster(clazz, multicaster);
+    }
+
+    /**
+     * Remove multicaster of event.
+     *
+     * @param clazz event
+     * @return whether success to remove
+     */
+    public boolean removerMulticaster(Class<? extends SentinelEvent> clazz) {
+        return eventMulticasterFactory.removeSentinelEventMulticaster(clazz);
+    }
+
+    /**
+     * Get multicaster of event.
+     *
+     * @param clazz event
+     * @return multicaster
+     */
+    public SentinelEventMulticaster getMulticaster(Class<? extends SentinelEvent> clazz) {
+        return eventMulticasterFactory.getSentinelEventMulticaster(clazz);
+    }
+
+    /**
+     * Publish event.
+     *
+     * @param event event
+     * @return whether success to publish
+     */
+    public boolean publish(SentinelEvent event) {
+        return eventMulticasterFactory.getSentinelEventMulticaster(event.getClass()).publish(event);
+    }
+
+}
---
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/event/SentinelEventListener.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 1999-2024 Alibaba Group Holding Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.alibaba.csp.sentinel.event;
+
+import com.alibaba.csp.sentinel.event.model.SentinelEvent;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Base listener.
+ *
+ * @author Daydreamer-ia
+ */
+public abstract class SentinelEventListener<T extends SentinelEvent> {
+
+    /**
+     * Min priority.
+     */
+    public static final Integer MIN_PRIORITY = Integer.MAX_VALUE;
+
+    /**
+     * Max priority.
+     */
+    public static final Integer MAX_PRIORITY = Integer.MIN_VALUE;
+
+    /**
+     * Callback when event published.
+     *
+     * @param event event msg.
+     */
+    public abstract void onEvent(T event);
+
+    /**
+     * Events of interest to the listener
+     */
+    public abstract Class<T> eventType();
+
+    /**
+     * Whether current listener handle the event asynchronously.
+     */
+    public Executor executor() {
+        return null;
+    }
+
+    /**
+     * Whether current listener allow concurrent execution.
+     */
+    public boolean allowConcurrentExec() {
+        return true;
+    }
+
+    /**
+     * Should it always handle the latest events and ignore expired ones.
+     */
+    public boolean alwaysLast() {
+        return false;
+    }
+
+    /**
+     * Priority, the less num, the more priority.
+     */
+    public int order() {
+        return MIN_PRIORITY;
+    }
+}
+
