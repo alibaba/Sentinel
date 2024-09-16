545de2b77364a4d71d4543ddcbdaf7cdab72574c
Daydreamer-ia <2296032269@qq.com>
2024-09-16 21:37:59 +0800

    code conventions

--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/event/factory/DefaultSentinelEventMulticasterFactory.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/event/factory/DefaultSentinelEventMulticasterFactory.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.alibaba.csp.sentinel.event.factory;
 
 import com.alibaba.csp.sentinel.event.model.SentinelEvent;
--- sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/event/BlockEventPublisher.java
+++ sentinel-core/src/main/java/com/alibaba/csp/sentinel/slots/event/BlockEventPublisher.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.alibaba.csp.sentinel.slots.event;
 
 import com.alibaba.csp.sentinel.event.SentinelEventBus;
